d2c29f739c40a1bbcec6ca9e14856c55bbfa6891
Jinyoung <glitt5384@naver.com>
2026-01-08 21:23:45 +0900

    initial commit

---
+++ src/main/java/com/sidework/SideWorkApplication.java
@@ -0,0 +1,13 @@
+package com.sidework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidework/SideWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
