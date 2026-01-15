c48e0adf3696409d6c7f762cadb29f95a4893e91
Jinyoung <glitt5384@naver.com>
2026-01-16 00:34:30 +0900

    feat: 유저 영속성 레포

---
+++ main-modules/user/infrastructure/src/main/java/com/sidework/user/persistence/repository/UserJpaRepository.java
@@ -0,0 +1,9 @@
+package com.sidework.user.persistence.repository;
+
+import com.sidework.user.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+}
