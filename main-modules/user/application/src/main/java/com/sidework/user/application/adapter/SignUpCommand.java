36ceb60804ca2bd4f5638092c6de893267f8be72
Jinyoung <glitt5384@naver.com>
2026-01-16 23:01:41 +0900

    feat: 회원가입 요청 객체

--- main-modules/user/application/src/main/java/com/sidework/user/application/adapter/SignUpCommand.java
+++ main-modules/user/application/src/main/java/com/sidework/user/application/adapter/SignUpCommand.java
@@ -1,11 +1,15 @@
 package com.sidework.user.application.adapter;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record SignUpCommand(
-        String email,
-        String password,
-        String name,
-        String nickname,
-        int age,
-        String tel
+        @NotNull @Email String email,
+        @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.") String password,
+        @NotNull String name,
+        @NotNull String nickname,
+        @NotNull Integer age,
+        @NotNull String tel
 ) {
 }
