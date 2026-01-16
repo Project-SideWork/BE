c9835c4bfb8ddeb1efe96ac7549388dd13c11e21
Jinyoung <glitt5384@naver.com>
2026-01-16 23:03:26 +0900

    feat: 유저 컨트롤러

---
+++ main-modules/user/application/src/main/java/com/sidework/user/application/adapter/UserController.java
@@ -0,0 +1,30 @@
+package com.sidework.user.application.adapter;
+
+import com.sidework.common.response.ApiResponse;
+import com.sidework.user.application.port.in.UserCommandUseCase;
+import com.sidework.user.application.port.in.UserQueryUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserCommandUseCase commandService;
+    private final UserQueryUseCase queryService;
+
+    @GetMapping("/email")
+    public ResponseEntity<ApiResponse<Boolean>> getEmailAvailable(@RequestParam String email) {
+        return ResponseEntity.ok(ApiResponse.onSuccess(queryService.checkEmailAvailable(email)));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> postNewUser(@Validated @RequestBody SignUpCommand command) {
+        commandService.signUp(command);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.onSuccessVoid());
+    }
+
+}
