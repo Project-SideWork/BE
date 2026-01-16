5242af573f97694177399b73bb571a66a0217346
Jinyoung <glitt5384@naver.com>
2026-01-16 22:51:55 +0900

    feat: 유효하지 않은 요청 객체 예외

---
+++ main-modules/user/application/src/main/java/com/sidework/user/application/exception/InvalidCommandException.java
@@ -0,0 +1,10 @@
+package com.sidework.user.application.exception;
+
+import com.sidework.common.response.exception.GlobalException;
+import com.sidework.common.response.status.ErrorStatus;
+
+public class InvalidCommandException extends GlobalException {
+    public InvalidCommandException() {
+        super(ErrorStatus.BAD_REQUEST);
+    }
+}
