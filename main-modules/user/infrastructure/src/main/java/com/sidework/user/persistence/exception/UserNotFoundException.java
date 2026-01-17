d124720a04f81c34c594ac78b991ba371af87a22
Jinyoung <glitt5384@naver.com>
2026-01-17 13:20:52 +0900

    feat: 조회 대상 사용자가 존재하지 않을 때 예외

---
+++ main-modules/user/infrastructure/src/main/java/com/sidework/user/persistence/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.sidework.user.persistence.exception;
+
+import com.sidework.common.response.exception.GlobalException;
+import com.sidework.common.response.status.ErrorStatus;
+
+public class UserNotFoundException extends GlobalException {
+    public UserNotFoundException(Long id) {
+        super(ErrorStatus.USER_NOT_FOUND.withDetail(String.format("사용자(id=%d)를 찾을 수 없습니다.", id)));
+    }
+}
