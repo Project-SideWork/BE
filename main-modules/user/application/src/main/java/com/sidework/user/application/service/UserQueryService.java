4b11ce51ce19689a4815362efe98c1f90b811c95
Jinyoung <glitt5384@naver.com>
2026-01-17 22:54:00 +0900

    feat: Transactional(readonly=true) 추가

--- main-modules/user/application/src/main/java/com/sidework/user/application/service/UserQueryService.java
+++ main-modules/user/application/src/main/java/com/sidework/user/application/service/UserQueryService.java
@@ -4,9 +4,11 @@ import com.sidework.user.application.port.in.UserQueryUseCase;
 import com.sidework.user.application.port.out.UserOutPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserQueryService implements UserQueryUseCase {
     private final UserOutPort userRepository;
 
