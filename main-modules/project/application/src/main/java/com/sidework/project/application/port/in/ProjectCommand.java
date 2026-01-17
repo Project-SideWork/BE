235ffef8c8810c0115ba3d85264fb7cc741b11b2
Jinyoung <glitt5384@naver.com>
2026-01-17 22:04:30 +0900

    feat: 프로젝트 생성 요청 dto

---
+++ main-modules/project/application/src/main/java/com/sidework/project/application/port/in/ProjectCommand.java
@@ -0,0 +1,42 @@
+package com.sidework.project.application.port.in;
+
+import com.sidework.project.domain.MeetingType;
+import com.sidework.project.domain.ProjectStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record ProjectCommand(
+
+        @NotBlank
+        @Size(max = 100)
+        String title,
+
+        @NotBlank
+        String description,
+
+        @NotNull
+        List<RecruitPosition> recruitPositions,
+
+        @NotNull
+        LocalDate startDt,
+
+        @NotNull
+        LocalDate endDt,
+
+        @NotNull
+        MeetingType meetingType,
+
+        String meetingDetail,
+
+        @NotNull
+        List<String> requiredStacks,
+
+        List<String> preferredStacks,
+
+        @NotNull
+        ProjectStatus status
+) {}
