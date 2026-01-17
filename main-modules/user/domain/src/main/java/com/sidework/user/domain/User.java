72635ed60c845ce399faa60662dc7feb2e6c339a
Jinyoung <glitt5384@naver.com>
2026-01-17 22:01:55 +0900

    fix: 빌더 어노테이션 삭제

--- main-modules/user/domain/src/main/java/com/sidework/user/domain/User.java
+++ main-modules/user/domain/src/main/java/com/sidework/user/domain/User.java
@@ -1,12 +1,10 @@
 package com.sidework.user.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
