46e1f1a797099fbca953c77935b8f6a6f70cbec6
Jinyoung <glitt5384@naver.com>
2026-01-18 21:01:14 +0900

    feat: 프로젝트 NOT FOUND 예외처리

--- main-modules/common/src/main/java/com/sidework/common/response/status/ErrorStatus.java
+++ main-modules/common/src/main/java/com/sidework/common/response/status/ErrorStatus.java
@@ -21,8 +21,11 @@ public enum ErrorStatus implements BaseStatusCode {
 
     // USER
     USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "USER_001", "이미 존재하는 사용자입니다."),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER_002", "해당 사용자를 찾을 수 없습니다."),
-    EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "USER_003", "이미 사용 중인 이메일입니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_002", "해당 사용자를 찾을 수 없습니다."),
+    EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "USER_003", "이미 사용 중인 이메일입니다."),
+
+    // PROJECT
+    PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "PROJECT_001", "해당 프로젝트를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
