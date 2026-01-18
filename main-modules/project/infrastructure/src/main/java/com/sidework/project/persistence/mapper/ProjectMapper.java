cc86bfe4b93fb0adf5c333f65c4b85bd4ceba4c7
Jinyoung <glitt5384@naver.com>
2026-01-18 21:01:33 +0900

    feat: instant <-> ldt

--- main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/mapper/ProjectMapper.java
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/mapper/ProjectMapper.java
@@ -4,8 +4,27 @@ import com.sidework.project.domain.Project;
 import com.sidework.project.persistence.entity.ProjectEntity;
 import org.mapstruct.Mapper;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 @Mapper(componentModel = "spring")
 public interface ProjectMapper {
+
+    ProjectEntity toEntity(Project project);
     Project toDomain(ProjectEntity entity);
-    ProjectEntity toEntity(Project domain);
+
+    default Instant map(LocalDate value) {
+        if (value == null) return null;
+        return value
+                .atStartOfDay(ZoneId.systemDefault())
+                .toInstant();
+    }
+
+    default LocalDate map(Instant value) {
+        if (value == null) return null;
+        return value
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
 }
