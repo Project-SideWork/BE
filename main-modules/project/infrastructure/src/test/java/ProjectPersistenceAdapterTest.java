c9c3a812c3881c76b2ccbd18f13e4e76aecd4ccc
Jinyoung <glitt5384@naver.com>
2026-01-18 21:02:07 +0900

    feat: 생성, 변경, 삭제 테스트

--- main-modules/project/infrastructure/src/test/java/ProjectPersistenceAdapterTest.java
+++ main-modules/project/infrastructure/src/test/java/ProjectPersistenceAdapterTest.java
@@ -7,20 +7,26 @@ import com.sidework.project.domain.Project;
 import com.sidework.project.domain.ProjectStatus;
 import com.sidework.project.persistence.adapter.ProjectPersistenceAdapter;
 import com.sidework.project.persistence.entity.ProjectEntity;
+import com.sidework.project.persistence.exception.ProjectNotFoundException;
 import com.sidework.project.persistence.mapper.ProjectMapper;
 import com.sidework.project.persistence.repository.ProjectJpaRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mapstruct.factory.Mappers;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ProjectPersistenceAdapterTest {
@@ -33,13 +39,68 @@ public class ProjectPersistenceAdapterTest {
     void setUp() {
         adapter = new ProjectPersistenceAdapter(repo, mapper);
     }
-
-
+    
     @Test
     void save는_도메인_객체를_영속성_객체로_변환해_저장한다() {
         Project domain = createProject(createCommand());
         adapter.save(domain);
         verify(repo).save(any(ProjectEntity.class));
+    }
+
+    @Test
+    void findById는_Id로_프로젝트를_조회해_도메인_객체로_변환한다() {
+        ProjectEntity entity = createProjectEntity();
+        when(repo.findById(1L)).thenReturn(Optional.of(entity));
+
+        Project project = adapter.findById(1L);
+
+        assertNotNull(project);
+        assertEquals(1L, project.getId());
+
+        verify(repo).findById(1L);
+    }
+
+    @Test
+    void findById로_존재하지_않는_프로젝트_조회_시_ProjectNotFoundException을_던진다(){
+        when(repo.findById(2L)).thenReturn(Optional.empty());
+        assertThrows(ProjectNotFoundException.class,
+                () -> adapter.findById(2L));
+
+        verify(repo).findById(2L);
+    }
+
+    @Test
+    void update는_기존_필드의_값을_덮어써_저장한다(){
+        ProjectEntity entity = createProjectEntity();
+        when(repo.findById(1L)).thenReturn(Optional.of(entity));
+
+        Project domain = adapter.findById(1L);
+
+        ProjectCommand updateCmd = createUpdateCommand();
+        domain.update(updateCmd.title(), updateCmd.description(), updateCmd.startDt(),
+                updateCmd.endDt(), updateCmd.meetingType(), updateCmd.status());
+
+        adapter.save(domain);
+
+        ArgumentCaptor<ProjectEntity> captor = ArgumentCaptor.forClass(ProjectEntity.class);
+        verify(repo).save(captor.capture());
+
+        ProjectEntity updated = captor.getValue();
+        assertNotEquals(entity.getTitle(), updated.getTitle());
+    }
+
+    @Test
+    void delete는_status값을_CANCELLED로_바꾼다(){
+        Project domain = createProject(createCommand());
+        domain.delete();
+
+        adapter.save(domain);
+        ArgumentCaptor<ProjectEntity> captor = ArgumentCaptor.forClass(ProjectEntity.class);
+
+        verify(repo).save(captor.capture());
+
+        ProjectEntity entity = captor.getValue();
+        assertEquals(ProjectStatus.CANCELED, entity.getStatus());
     }
 
     private ProjectCommand createCommand() {
@@ -67,6 +128,32 @@ public class ProjectPersistenceAdapterTest {
                 ProjectStatus.RECRUITING          // status
         );
     }
+
+    private ProjectCommand createUpdateCommand() {
+        return new ProjectCommand(
+                "AI 기반 관광 코스 추천 서비스",              // title
+                "사용자 위치와 혼잡도를 반영한 여행 코스 추천", // description
+                List.of(
+                        new RecruitPosition(
+                                RecruitRole.BACKEND,
+                                2,
+                                SkillLevel.MID
+                        ),
+                        new RecruitPosition(
+                                RecruitRole.FRONTEND,
+                                1,
+                                SkillLevel.JUNIOR
+                        )
+                ),
+                LocalDate.of(2025, 4, 1),   // startDt
+                LocalDate.of(2025, 7, 31),  // endDt
+                MeetingType.ONLINE,         // meetingType
+                "전면 온라인, 필요 시 비동기 협업", // meetingDetail
+                List.of("Spring Boot", "MongoDB", "GraphQL"), // requiredStacks
+                List.of("Redis", "Docker", "AWS"),            // preferredStacks
+                ProjectStatus.PREPARING                         // status
+        );
+    }
     private Project createProject(
             ProjectCommand command
     ) {
@@ -78,6 +165,17 @@ public class ProjectPersistenceAdapterTest {
                 command.endDt(),
                 command.meetingType(),
                 command.status()
+        );
+    }
+    private ProjectEntity createProjectEntity() {
+        return new ProjectEntity(
+                1L,
+                "버스 실시간 위치 서비스",              // title
+                "사용자 위치와 혼잡도를 반영한 여행 코스 추천", // description
+                Instant.now(),
+                Instant.now(),
+                MeetingType.HYBRID,
+                ProjectStatus.CANCELED
         );
     }
 }
