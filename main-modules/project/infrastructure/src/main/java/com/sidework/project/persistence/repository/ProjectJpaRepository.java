c08451a5056d8ca2774d54cee3c6231b3a4abdee
Jinyoung <glitt5384@naver.com>
2026-01-19 16:06:56 +0900

    feat: 프로젝트 id 배열로 제목 배열 반환

--- main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectJpaRepository.java
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectJpaRepository.java
@@ -2,8 +2,19 @@ package com.sidework.project.persistence.repository;
 
 import com.sidework.project.persistence.entity.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProjectJpaRepository extends JpaRepository<ProjectEntity, Long> {
+    @Query(
+            """
+            SELECT p.title FROM ProjectEntity p
+            WHERE p.id IN (:ids)
+            """
+    )
+    List<String> findAllTitlesByIds(@Param("ids") List<Long> ids);
 }
