daa3a59529b066e8a181f3c8a841e433a546b365
Jinyoung <glitt5384@naver.com>
2026-01-19 16:26:18 +0900

    feat: 예외에 세부 정보 기입

--- main-modules/project/application/src/main/java/com/sidework/project/application/exception/InvalidCommandException.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/exception/InvalidCommandException.java
@@ -4,8 +4,8 @@ import com.sidework.common.response.exception.GlobalException;
 import com.sidework.common.response.status.ErrorStatus;
 
 public class InvalidCommandException extends GlobalException {
-    public InvalidCommandException() {
-        super(ErrorStatus.BAD_REQUEST);
+    public InvalidCommandException(String detail) {
+        super(ErrorStatus.BAD_REQUEST.withDetail(detail));
     }
 }
 
