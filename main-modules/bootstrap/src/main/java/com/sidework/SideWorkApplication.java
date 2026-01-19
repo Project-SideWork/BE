45cad9cef55e310d5b39c06132d837f1a637d5b0
Jinyoung <glitt5384@naver.com>
2026-01-19 16:09:48 +0900

    feat: DomainJpaConfig 대신 Application에서 지정

--- main-modules/bootstrap/src/main/java/com/sidework/SideWorkApplication.java
+++ main-modules/bootstrap/src/main/java/com/sidework/SideWorkApplication.java
@@ -2,8 +2,14 @@ package com.sidework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.sidework"})
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {"com.sidework.project.persistence.repository", "com.sidework.user.persistence.repository"})
+@EntityScan(basePackages = {"com.sidework.project.persistence.entity", "com.sidework.user.persistence.entity"})
 public class SideWorkApplication {
 
 	public static void main(String[] args) {
