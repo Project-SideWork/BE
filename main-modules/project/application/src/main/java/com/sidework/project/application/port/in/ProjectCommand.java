83d86f1a54387fbc2a979e44c0d3f4a844e9d503
Jinyoung <glitt5384@naver.com>
2026-01-19 16:26:28 +0900

    feat: Enum 배열에 @Valid 추가

--- main-modules/project/application/src/main/java/com/sidework/project/application/port/in/ProjectCommand.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/port/in/ProjectCommand.java
@@ -4,6 +4,7 @@ import com.sidework.project.domain.MeetingType;
 import com.sidework.project.domain.Project;
 import com.sidework.project.domain.ProjectRole;
 import com.sidework.project.domain.ProjectStatus;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -24,7 +25,7 @@ public record ProjectCommand(
         ProjectRole myRole,
 
         @NotNull
-        List<RecruitPosition> recruitPositions,
+        List<@Valid RecruitPosition> recruitPositions,
 
         @NotNull
         LocalDate startDt,
