1ef82420d8ab85caad2d044a2833fc34d34ca8c9
Jinyoung <glitt5384@naver.com>
2026-01-19 16:08:03 +0900

    feat: existsById, findAllTitles 추가 및 save 반환 구현

--- main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/adapter/ProjectPersistenceAdapter.java
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/adapter/ProjectPersistenceAdapter.java
@@ -1,14 +1,15 @@
 package com.sidework.project.persistence.adapter;
 
-import com.sidework.project.application.port.in.ProjectCommand;
 import com.sidework.project.application.port.out.ProjectOutPort;
 import com.sidework.project.domain.Project;
+import com.sidework.project.application.exception.ProjectNotFoundException;
 import com.sidework.project.persistence.entity.ProjectEntity;
-import com.sidework.project.persistence.exception.ProjectNotFoundException;
 import com.sidework.project.persistence.mapper.ProjectMapper;
 import com.sidework.project.persistence.repository.ProjectJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -17,13 +18,25 @@ public class ProjectPersistenceAdapter implements ProjectOutPort {
     private final ProjectMapper mapper;
 
     @Override
-    public void save(Project project) {
-        repo.save(mapper.toEntity(project));
+    public void existsById(Long projectId) {
+          if(!repo.existsById(projectId)) {
+              throw new ProjectNotFoundException(projectId);
+          }
+    }
+
+    @Override
+    public Long save(Project project) {
+        return repo.save(mapper.toEntity(project)).getId();
     }
 
     @Override
     public Project findById(Long id) {
         ProjectEntity entity = repo.findById(id).orElseThrow(() -> new ProjectNotFoundException(id));
         return mapper.toDomain(entity);
+    }
+
+    @Override
+    public List<String> findAllTitles(List<Long> projectIds) {
+        return repo.findAllTitlesByIds(projectIds);
     }
 }
