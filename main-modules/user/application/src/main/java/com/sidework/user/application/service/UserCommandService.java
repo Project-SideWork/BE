d03b61ba95871b91e59830428640f1fd3783855a
Jinyoung <glitt5384@naver.com>
2026-01-19 16:10:08 +0900

    fix: @Validated와 중복 책임이라 삭제

--- main-modules/user/application/src/main/java/com/sidework/user/application/service/UserCommandService.java
+++ main-modules/user/application/src/main/java/com/sidework/user/application/service/UserCommandService.java
@@ -20,14 +20,6 @@ public class UserCommandService implements UserCommandUseCase {
 
     @Override
     public void signUp(SignUpCommand command) {
-        if(command == null) {
-            throw new InvalidCommandException();
-        }
-
-        if(command.email() == null || command.name() == null || command.nickname() == null
-        || command.password() == null || command.tel() == null || command.age() == null) {
-            throw new InvalidCommandException();
-        }
         User user = User.create(command.email(), command.name(), command.nickname(), encodePassword(command.password())
                 , command.age(), command.tel(), UserType.LOCAL);
         userRepository.save(user);
