8a84445c1c14c92389cd5b265e53d31db4906782
Jinyoung <glitt5384@naver.com>
2026-01-19 16:12:16 +0900

    feat: 프로젝트 CUD API

---
+++ main-modules/project/application/src/main/java/com/sidework/project/application/adapter/ProjectController.java
@@ -0,0 +1,39 @@
+package com.sidework.project.application.adapter;
+
+import com.sidework.common.response.ApiResponse;
+import com.sidework.project.application.port.in.ProjectCommand;
+import com.sidework.project.application.port.in.ProjectCommandUseCase;
+import com.sidework.project.application.port.in.ProjectQueryUseCase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectCommandUseCase commandService;
+    private final ProjectQueryUseCase queryService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> postNewProject(@Validated @RequestBody ProjectCommand command) {
+        commandService.create(command);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.onSuccessCreated());
+    }
+
+    @PatchMapping("/{projectId}")
+    public ResponseEntity<ApiResponse<Void>> patchProject(@PathVariable("projectId") Long projectId, @Validated @RequestBody ProjectCommand command) {
+        commandService.update(projectId, command);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.onSuccessVoid());
+    }
+
+    // TODO: UserDetail 변경
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<ApiResponse<Void>> deleteProject(@PathVariable("projectId") Long projectId) {
+        commandService.delete(1L, projectId);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.onSuccessVoid());
+    }
+}
