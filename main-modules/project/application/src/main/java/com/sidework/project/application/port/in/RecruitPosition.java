15f460411f8d05d6703f1c61e78e0675af577164
Jinyoung <glitt5384@naver.com>
2026-01-19 16:09:13 +0900

    feat: 모집 직군 Vo

--- main-modules/project/application/src/main/java/com/sidework/project/application/port/in/RecruitPosition.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/port/in/RecruitPosition.java
@@ -1,13 +1,15 @@
 package com.sidework.project.application.port.in;
 
-import jakarta.validation.constraints.NotBlank;
+import com.sidework.project.domain.ProjectRole;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record RecruitPosition(
-        @NotBlank
-        RecruitRole role,       // 프론트엔드, 백엔드 등
+        @NotNull
+        ProjectRole role,       // 프론트엔드, 백엔드 등
 
         @NotNull
+        @Min(value = 1, message = "모집 인원은 0보다 커야합니다.")
         Integer headCount, // 인원 수
 
         @NotNull
