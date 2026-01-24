bb138850033a7392b868b19e9c69677d95546294
Gijin Park <dugudada01@naver.com>
2026-01-24 23:09:01 +0900

    feat: ProfileJpaRepository 구현

---
+++ main-modules/profile/infrastructure/src/main/java/com/sidework/profile/persistence/repository/ProfileJpaRepository.java
@@ -0,0 +1,13 @@
+package com.sidework.profile.persistence.repository;
+
+import com.sidework.profile.persistence.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProfileJpaRepository extends JpaRepository<ProfileEntity, Long> {
+	Optional<ProfileEntity> findByUserId(Long userId);
+	boolean existsByUserId(Long userId);
+}
