c9e12dc1f1a68dcb33cf302f55186a4cf30aef3a
Gijin Park <dugudada01@naver.com>
2026-01-24 23:11:35 +0900

    feat: Mapper 추가

---
+++ main-modules/profile/infrastructure/src/main/java/com/sidework/profile/persistence/mapper/ProfileRoleMapper.java
@@ -0,0 +1,14 @@
+package com.sidework.profile.persistence.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.sidework.profile.domain.ProfileRole;
+import com.sidework.profile.persistence.entity.ProfileRoleEntity;
+import com.sidework.profile.persistence.entity.RoleEntity;
+
+@Mapper(componentModel = "spring")
+public interface ProfileRoleMapper
+{
+	ProfileRole toDomain(ProfileRoleEntity entity);
+	ProfileRoleEntity (ProfileRole domain);
+}
---
+++ main-modules/profile/infrastructure/src/main/java/com/sidework/profile/persistence/mapper/ProfileSkillMapper.java
@@ -0,0 +1,14 @@
+package com.sidework.profile.persistence.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.sidework.profile.domain.ProfileSchool;
+import com.sidework.profile.domain.ProfileSkill;
+import com.sidework.profile.persistence.entity.ProfileSchoolEntity;
+import com.sidework.profile.persistence.entity.ProfileSkillEntity;
+
+@Mapper(componentModel = "spring")
+public interface ProfileSkillMapper {
+	ProfileSkill toDomain(ProfileSkillEntity entity);
+	ProfileSkillEntity toEntity(ProfileSkill domain);
+}
