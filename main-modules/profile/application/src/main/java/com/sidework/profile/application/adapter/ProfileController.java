0eb072a6aae0dda003363090ba22735c7df943f8
Gijin Park <dugudada01@naver.com>
2026-01-26 20:58:40 +0900

    feat: 프로필 수정 컨트롤러 구현

--- main-modules/profile/application/src/main/java/com/sidework/profile/application/adapter/ProfileController.java
+++ main-modules/profile/application/src/main/java/com/sidework/profile/application/adapter/ProfileController.java
@@ -1,25 +1,39 @@
 package com.sidework.profile.application.adapter;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sidework.common.response.ApiResponse;
+import com.sidework.profile.application.port.in.ProfileCommandUseCase;
 import com.sidework.profile.application.port.in.ProfileQueryUseCase;
+import com.sidework.profile.application.port.in.ProfileUpdateCommand;
 
 import lombok.RequiredArgsConstructor;
+
+//TODO: 로그인 연동
 
 @RestController
 @RequestMapping("/api/v1/profiles")
 @RequiredArgsConstructor
 public class ProfileController {
 	private final ProfileQueryUseCase queryService;
+	private final ProfileCommandUseCase commandService;
 
-	//TODO: 로그인 연동
 	@GetMapping
 	public ResponseEntity<ApiResponse<UserProfileResponse>> getUserProfile()
 	{
 		return ResponseEntity.ok(ApiResponse.onSuccess(queryService.getProfileByUserId(1L)));
+	}
+
+	@PatchMapping
+	public ResponseEntity<ApiResponse<Void>> updateUserProfile(@RequestBody ProfileUpdateCommand profileUpdateCommand)
+	{
+		commandService.update(1L, profileUpdateCommand);
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.onSuccessVoid());
 	}
 }
