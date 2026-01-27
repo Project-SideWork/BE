ada262784c0aaffa6e03dc73eb38c6374bbcacad
Jinyoung <glitt5384@naver.com>
2026-01-27 20:37:12 +0900

    feat: 프로젝트 우대 기술 생성 및 수정 구현

---
+++ main-modules/skill/application/src/main/java/com/sidework/skill/application/service/ProjectPreferredSkillCommandService.java
@@ -0,0 +1,91 @@
+package com.sidework.skill.application.service;
+
+
+import com.sidework.common.exception.InvalidCommandException;
+import com.sidework.skill.application.port.in.ProjectPreferredSkillCommand;
+import com.sidework.skill.application.port.in.ProjectPreferredSkillCommandUseCase;
+import com.sidework.skill.application.port.out.ProjectPreferredSkillOutPort;
+import com.sidework.skill.application.port.out.SkillOutPort;
+import com.sidework.skill.domain.ProjectPreferredSkill;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = false)
+public class ProjectPreferredSkillCommandService implements ProjectPreferredSkillCommandUseCase {
+    private final ProjectPreferredSkillOutPort repo;
+    private final SkillOutPort skillRepo;
+
+    @Override
+    public void create(Long projectId, ProjectPreferredSkillCommand command) {
+        List<ProjectPreferredSkill> domains = convert(projectId, command.skillIds());
+        repo.saveAll(domains);
+    }
+
+    @Override
+    public void update(Long projectId, ProjectPreferredSkillCommand command) {
+        Pair<List<ProjectPreferredSkill>, List<ProjectPreferredSkill>> categorized = categorize(projectId, command.skillIds());
+        repo.saveAll(categorized.getLeft());
+        repo.deleteAll(categorized.getRight());
+    }
+
+    private List<ProjectPreferredSkill> convert(Long projectId, List<Long> skillIds) {
+        return skillIds.stream()
+                .map(id -> {
+                        if(!skillRepo.existsById(id)) {
+                            throw new InvalidCommandException(String.format("추가하고자 하는 기술(id=%d)을 찾을 수 없습니다.", id));
+                        }
+                        return ProjectPreferredSkill.builder()
+                                    .projectId(projectId)
+                                    .skillId(id)
+                                    .build();
+                }
+                ).toList();
+    }
+
+    private Pair<List<ProjectPreferredSkill>, List<ProjectPreferredSkill>> categorize(Long projectId, List<Long> skillIds) {
+        // 원래 저장되어 있었던 ProjectPreferredSkill의 Skill ID 목록.
+        Set<Long> originalIds = new HashSet<>(
+                repo.findAllSkillIdsByProject(projectId)
+        );
+
+        Set<Long> requestedIds = new HashSet<>(skillIds);
+
+        Set<Long> existingSkillIds = new HashSet<>(skillRepo.findIdsByIdIn(requestedIds.stream().toList()));
+
+        Set<Long> missing = requestedIds.stream()
+                .filter(id -> !existingSkillIds.contains(id))
+                .collect(Collectors.toSet());
+
+        if (!missing.isEmpty()) {
+            throw new InvalidCommandException("존재하지 않는 기술 id: " + missing);
+        }
+
+        List<ProjectPreferredSkill> toAdd = requestedIds.stream()
+                .filter(id -> !originalIds.contains(id))
+                .map(id -> ProjectPreferredSkill.builder()
+                        .projectId(projectId)
+                        .skillId(id)
+                        .build()
+                )
+                .toList();
+
+        List<ProjectPreferredSkill> toRemove = originalIds.stream()
+                .filter(id -> !requestedIds.contains(id))
+                .map(id -> ProjectPreferredSkill.builder()
+                        .projectId(projectId)
+                        .skillId(id)
+                        .build()
+                )
+                .toList();
+        return Pair.of(toAdd, toRemove);
+    }
+}
