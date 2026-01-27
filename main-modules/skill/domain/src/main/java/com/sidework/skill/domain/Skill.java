24a8fcb2dc90a73b241be7abd49411941b5a9724
Jinyoung <glitt5384@naver.com>
2026-01-27 20:38:51 +0900

    feat: 스킬 도메인 객체

---
+++ main-modules/skill/domain/src/main/java/com/sidework/skill/domain/Skill.java
@@ -0,0 +1,13 @@
+package com.sidework.skill.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Skill {
+    private Long id;
+    private String name;
+}
