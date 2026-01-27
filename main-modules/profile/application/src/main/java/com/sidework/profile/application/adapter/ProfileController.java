fcac039a8820cfb90adb54d5c2d3cb0ef396cb6e
Gijin Park <dugudada01@naver.com>
2026-01-27 15:01:28 +0900

    fix: putmapping으로 변경

--- main-modules/profile/application/src/main/java/com/sidework/profile/application/adapter/ProfileController.java
+++ main-modules/profile/application/src/main/java/com/sidework/profile/application/adapter/ProfileController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +31,7 @@ public class ProfileController {
 		return ResponseEntity.ok(ApiResponse.onSuccess(queryService.getProfileByUserId(1L)));
 	}
 
-	@PatchMapping
+	@PutMapping
 	public ResponseEntity<ApiResponse<Void>> updateUserProfile(@RequestBody ProfileUpdateCommand profileUpdateCommand)
 	{
 		commandService.update(1L, profileUpdateCommand);
