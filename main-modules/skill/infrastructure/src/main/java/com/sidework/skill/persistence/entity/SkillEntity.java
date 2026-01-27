3850853526cbcd64f731bbacda1dba8808f26291
Jinyoung <glitt5384@naver.com>
2026-01-27 20:38:56 +0900

    feat: 스킬 영속성 객체

---
+++ main-modules/skill/infrastructure/src/main/java/com/sidework/skill/persistence/entity/SkillEntity.java
@@ -0,0 +1,24 @@
+package com.sidework.skill.persistence.entity;
+
+import com.sidework.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "skills")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SkillEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
