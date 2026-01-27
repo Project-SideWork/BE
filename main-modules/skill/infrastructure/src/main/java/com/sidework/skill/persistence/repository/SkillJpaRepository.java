35c4b3d5072ad677045c1a0def18fa7440dc132a
Jinyoung <glitt5384@naver.com>
2026-01-27 20:39:04 +0900

    feat: 스킬 레포지토리

---
+++ main-modules/skill/infrastructure/src/main/java/com/sidework/skill/persistence/repository/SkillJpaRepository.java
@@ -0,0 +1,17 @@
+package com.sidework.skill.persistence.repository;
+
+import com.sidework.skill.persistence.entity.SkillEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SkillJpaRepository extends JpaRepository<SkillEntity, Long> {
+    List<SkillEntity> findByIdIn(List<Long> ids);
+
+    @Query("select s.id from SkillEntity s where s.id in :ids")
+    List<Long> findAllIdsByIdIn(@Param("ids") List<Long> ids);
+}
