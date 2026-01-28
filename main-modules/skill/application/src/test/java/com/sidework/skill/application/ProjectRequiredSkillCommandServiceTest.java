b3519153762fa2753f8c8504ba9f9504aab6d947
Jinyoung <glitt5384@naver.com>
2026-01-28 17:58:02 +0900

    feat: 프로젝트 필수 기술 생성 구현체 테스트

---
+++ main-modules/skill/application/src/test/java/com/sidework/skill/application/ProjectRequiredSkillCommandServiceTest.java
@@ -0,0 +1,96 @@
+package com.sidework.skill.application;
+
+
+import com.sidework.common.exception.InvalidCommandException;
+import com.sidework.skill.application.port.out.ProjectRequiredSkillOutPort;
+import com.sidework.skill.application.port.out.SkillOutPort;
+import com.sidework.skill.application.service.ProjectRequiredSkillCommandService;
+import com.sidework.skill.domain.ProjectRequiredSkill;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProjectRequiredSkillCommandServiceTest {
+    @Mock
+    private ProjectRequiredSkillOutPort repo;
+
+    @Mock
+    private SkillOutPort skillRepo;
+
+    @InjectMocks
+    ProjectRequiredSkillCommandService service;
+
+    @Captor
+    ArgumentCaptor<List<ProjectRequiredSkill>> captor;
+
+    @Test
+    void 정상적인_프로젝트_우대기술_생성_DTO로_프로젝트_생성에_성공한다() {
+        List<Long> command = createCommand();
+        when(skillRepo.existsById(1L)).thenReturn(true);
+        when(skillRepo.existsById(2L)).thenReturn(true);
+        when(skillRepo.existsById(3L)).thenReturn(true);
+        service.create(1L, command);
+
+        verify(repo).saveAll(captor.capture());
+
+        List<ProjectRequiredSkill> saved = captor.getValue();
+        assertEquals(3, saved.size());
+    }
+
+    @Test
+    void DTO에_DB에_존재하지_않는_ID가_있으면_InvalidCommandException을_던진다() {
+        List<Long> command = createCommand();
+        when(skillRepo.existsById(1L)).thenReturn(true);
+        when(skillRepo.existsById(2L)).thenReturn(true);
+        when(skillRepo.existsById(3L)).thenReturn(false);
+        assertThrows(
+                InvalidCommandException.class, () -> service.create(1L, command)
+        );
+    }
+
+    @Test
+    void 프로젝트_수정시_신규기술_추가에_성공한다() {
+        List<Long> command = createCommand();
+        when(skillRepo.findIdsByIdIn(List.of(1L, 2L, 3L))).thenReturn(List.of(1L,2L,3L));
+        when(repo.findAllSkillIdsByProject(1L)).thenReturn(List.of(1L, 2L));
+        service.update(1L, command);
+
+        verify(repo).saveAll(captor.capture());
+
+        List<ProjectRequiredSkill> saved = captor.getValue();
+        assertEquals(1, saved.size());
+    }
+
+    @Test
+    void 프로젝트_수정시_요청에_미포함된_기존_기술_삭제에_성공한다() {
+        List<Long> command = createDeleteCommand();
+        when(skillRepo.findIdsByIdIn(List.of(1L, 2L))).thenReturn(List.of(1L,2L));
+        when(repo.findAllSkillIdsByProject(1L)).thenReturn(List.of(1L, 2L, 3L));
+        service.update(1L, command);
+
+        verify(repo).deleteAll(captor.capture());
+
+        List<ProjectRequiredSkill> deleted = captor.getValue();
+        assertEquals(1, deleted.size());
+    }
+
+    private List<Long> createCommand() {
+        return List.of(1L, 2L, 3L);
+    }
+
+    private List<Long> createDeleteCommand() {
+        return List.of(1L, 2L);
+    }
+}
