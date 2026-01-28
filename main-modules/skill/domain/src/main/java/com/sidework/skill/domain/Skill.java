2744b2b1979d6f257c888837beac09d081d9fbb8
Jinyoung <glitt5384@naver.com>
2026-01-28 18:09:19 +0900

    feat: 빌더 어노테이션 추가

--- main-modules/skill/domain/src/main/java/com/sidework/skill/domain/Skill.java
+++ main-modules/skill/domain/src/main/java/com/sidework/skill/domain/Skill.java
@@ -1,10 +1,12 @@
 package com.sidework.skill.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Skill {
