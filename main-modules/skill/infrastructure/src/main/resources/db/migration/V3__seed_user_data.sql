0290fef24410a23ce6a20aaf70f11d3db5511603
Jinyoung <glitt5384@naver.com>
2026-01-28 23:42:33 +0900

    fix: 비밀번호 실제 encrypt해서 추가

--- main-modules/skill/infrastructure/src/main/resources/db/migration/V3__seed_user_data.sql
+++ main-modules/skill/infrastructure/src/main/resources/db/migration/V3__seed_user_data.sql
@@ -11,7 +11,7 @@ INSERT INTO users (
     'test1@sidework.com',
     '홍길동',
     'gildong',
-    '$2a$10$abcdefghijklmnopqrstuv', -- bcrypt 더미
+    '$2a$10$VpMd918Li2F6LyorWG8y9u8KBvvZirMvBj3prFWZzVhyclxbYiO1C',
     25,
     '010-1234-5678',
     'USER',
