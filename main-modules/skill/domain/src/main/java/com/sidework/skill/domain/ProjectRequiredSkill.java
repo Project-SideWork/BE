d6292a45905ed495c74b8d8e074305029fcec43b
Jinyoung <glitt5384@naver.com>
2026-01-28 17:55:31 +0900

    feat: 프로젝트 우대 기술 도메인 객체

--- main-modules/skill/domain/src/main/java/com/sidework/skill/domain/ProjectRequiredSkill.java
+++ main-modules/skill/domain/src/main/java/com/sidework/skill/domain/ProjectRequiredSkill.java
@@ -1,5 +1,14 @@
 package com.sidework.skill.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectRequiredSkill {
     private Long id;
 
