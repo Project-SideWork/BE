a4ecd38b5dec9a1c61194226696d3c53e2ebb1f5
Gijin Park <dugudada01@naver.com>
2026-02-04 20:58:58 +0900

    feat: 알림 어뎁터 구현

---
+++ main-modules/notification/infrastructure/src/main/java/com/sidework/notification/persistence/adapter/NotificationPersistenceAdapter.java
@@ -0,0 +1,54 @@
+package com.sidework.notification.persistence.adapter;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sidework.notification.application.exception.NotificationNotFoundException;
+import com.sidework.notification.application.port.out.NotificationOutPort;
+import com.sidework.notification.domain.Notification;
+import com.sidework.notification.persistence.entity.NotificationEntity;
+import com.sidework.notification.persistence.mapper.NotificationMapper;
+import com.sidework.notification.persistence.repository.NotificationJpaRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationPersistenceAdapter implements NotificationOutPort {
+
+	private final NotificationMapper mapper;
+	private final NotificationJpaRepository repository;
+
+	@Override
+	public void save(Notification notification) {
+		if(notification == null) {
+			return;
+		}
+		repository.save(mapper.toEntity(notification));
+	}
+
+	@Override
+	public void update(Notification notification) {
+		if(notification == null) {
+			return;
+		}
+		NotificationEntity entity =  repository.findById(notification.getId())
+			.orElseThrow(() -> new NotificationNotFoundException(notification.getId()));
+		mapper.updateEntity(entity, notification);
+	}
+
+	@Override
+	public List<Notification> findByUserId(Long userId) {
+		return repository.findByUserId(userId).stream()
+			.map(mapper::toDomain)
+			.toList();
+	}
+
+	@Override
+	public Notification findById(Long id) {
+		return repository.findById(id)
+			.map(mapper::toDomain)
+			.orElse(null);
+	}
+}
---
+++ main-modules/notification/infrastructure/src/main/java/com/sidework/notification/persistence/adapter/NotificationSendPersistenceAdapter.java
@@ -0,0 +1,23 @@
+package com.sidework.notification.persistence.adapter;
+
+import org.springframework.stereotype.Component;
+
+import com.sidework.notification.application.adapter.NotificationResponse;
+import com.sidework.notification.application.port.out.NotificationSendOutPort;
+import com.sidework.notification.domain.Notification;
+import com.sidework.notification.persistence.sse.SseEmitterManager;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationSendPersistenceAdapter implements NotificationSendOutPort {
+
+	private final SseEmitterManager sseEmitterManager;
+
+	@Override
+	public void send(Long userId, Notification notification) {
+		NotificationResponse response = NotificationResponse.from(notification);
+		sseEmitterManager.sendToUser(userId, response);
+	}
+}
