9869a5baaf660384e7c925c21fa22d83ddbeb798
Gijin Park <dugudada01@naver.com>
2026-02-04 21:44:43 +0900

    feat: ping 이벤트 스케줄러 등록

--- main-modules/bootstrap/src/main/java/com/sidework/SideWorkApplication.java
+++ main-modules/bootstrap/src/main/java/com/sidework/SideWorkApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.sidework"})
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = {"com.sidework.**.persistence.repository"})
+@EnableScheduling
 @EntityScan(basePackages = {"com.sidework.project.persistence.entity", "com.sidework.user.persistence.entity",
         "com.sidework.profile.persistence.entity", "com.sidework.skill.persistence.entity",
         "com.sidework.notification.persistence.entity"})
