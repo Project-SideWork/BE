1ad25e598d559111f78ac5da363d8af6d1e388cd
Gijin Park <dugudada01@naver.com>
2026-02-09 15:50:00 +0900

    feat: findTokensByUserId 메서드 구현

--- main-modules/notification/application/src/main/java/com/sidework/notification/application/port/out/FcmTokenOutPort.java
+++ main-modules/notification/application/src/main/java/com/sidework/notification/application/port/out/FcmTokenOutPort.java
@@ -1,8 +1,12 @@
 package com.sidework.notification.application.port.out;
 
+import java.util.List;
+
 import com.sidework.notification.domain.FcmUserToken;
 
 public interface FcmTokenOutPort {
 
 	void registerToken(FcmUserToken fcmUserToken);
+
+	List<FcmUserToken> findTokensByUserId(Long userId);
 }
--- main-modules/notification/infrastructure/src/main/java/com/sidework/notification/persistence/adapter/FcmTokenPersistenceAdapter.java
+++ main-modules/notification/infrastructure/src/main/java/com/sidework/notification/persistence/adapter/FcmTokenPersistenceAdapter.java
@@ -44,4 +44,11 @@ public class FcmTokenPersistenceAdapter implements FcmTokenOutPort {
 				}
 			);
 	}
+
+	@Override
+	public List<FcmUserToken> findTokensByUserId(Long userId) {
+		return fcmTokenJpaRepository.findByUserIdAndPushAgreedTrue(userId).stream()
+			.map(fcmTokenMapper::toDomain)
+			.toList();
+	}
 }
