016b6d7919f03664eb7be83bcb49d214e15a2bb1
Jinyoung <glitt5384@naver.com>
2026-02-09 22:26:30 +0900

    feat: UserDetailsService 구현체

---
+++ main-modules/bootstrap/src/main/java/com/sidework/security/service/AuthenticatedUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.sidework.security.service;
+
+import com.sidework.common.auth.AuthenticatedUser;
+import com.sidework.common.auth.SecurityAuthenticatedUser;
+import com.sidework.security.dto.AuthenticatedUserDetails;
+import com.sidework.user.application.port.out.UserOutPort;
+import com.sidework.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthenticatedUserDetailsService implements UserDetailsService {
+    private final UserOutPort repo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findByEmail(username);
+        AuthenticatedUser authenticatedUser = new SecurityAuthenticatedUser(
+                user.getId(), user.getEmail(), user.getName()
+        );
+
+        return new AuthenticatedUserDetails(authenticatedUser, user.getPassword());
+    }
+}
