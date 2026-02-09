5f5895c3bbc138a8d1f13880d1745a52eb00dddc
Jinyoung <glitt5384@naver.com>
2026-02-09 22:25:36 +0900

    feat: 유저 404 + 이메일 예외 추가

--- main-modules/user/infrastructure/src/main/java/com/sidework/user/persistence/exception/UserNotFoundException.java
+++ main-modules/user/infrastructure/src/main/java/com/sidework/user/persistence/exception/UserNotFoundException.java
@@ -7,4 +7,8 @@ public class UserNotFoundException extends GlobalException {
     public UserNotFoundException(Long id) {
         super(ErrorStatus.USER_NOT_FOUND.withDetail(String.format("사용자(id=%d)를 찾을 수 없습니다.", id)));
     }
+
+    public UserNotFoundException(String email) {
+        super(ErrorStatus.USER_NOT_FOUND.withDetail(String.format("사용자(email=%d)를 찾을 수 없습니다.", email)));
+    }
 }
