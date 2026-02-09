1f520ec5d59f86e3de059eecff67f5bc528e0538
Gijin Park <dugudada01@naver.com>
2026-02-09 15:45:03 +0900

    feat: FirebaseConfig 등록

---
+++ main-modules/notification/infrastructure/src/main/java/com/sidework/notification/config/FirebaseConfig.java
@@ -0,0 +1,37 @@
+package com.sidework.notification.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+@Configuration
+public class FirebaseConfig {
+
+	// TODO: 배포환경 경로 시크릿에서 주입할 것
+	@Bean
+	public FirebaseMessaging firebaseMessaging() throws IOException {
+		InputStream serviceAccount =
+			new ClassPathResource("firebase.json").getInputStream();
+
+		FirebaseOptions options = FirebaseOptions.builder()
+			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+			.build();
+
+		FirebaseApp app;
+		if (FirebaseApp.getApps().isEmpty()) {
+			app = FirebaseApp.initializeApp(options);
+		} else {
+			app = FirebaseApp.getInstance();
+		}
+
+		return FirebaseMessaging.getInstance(app);
+	}
+}
