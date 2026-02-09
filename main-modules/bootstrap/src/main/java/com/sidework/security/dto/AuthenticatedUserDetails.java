68def0ebc5af244438b67e419141b3471ad0e833
Jinyoung <glitt5384@naver.com>
2026-02-09 22:26:17 +0900

    feat: UserDetails 구현체

---
+++ main-modules/bootstrap/src/main/java/com/sidework/security/dto/AuthenticatedUserDetails.java
@@ -0,0 +1,30 @@
+package com.sidework.security.dto;
+
+import com.sidework.common.auth.AuthenticatedUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class AuthenticatedUserDetails implements UserDetails {
+    private final AuthenticatedUser user;
+    private final String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+}
