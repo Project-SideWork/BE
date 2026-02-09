7c801581672d38754af2b0ff37aff37b50bdf02b
Jinyoung <glitt5384@naver.com>
2026-02-09 22:27:16 +0900

    feat: jwt 토큰 처리 필터

---
+++ main-modules/bootstrap/src/main/java/com/sidework/security/filter/JwtFilter.java
@@ -0,0 +1,85 @@
+package com.sidework.security.filter;
+
+import com.sidework.security.util.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    private static final List<String> EXCLUDE_URLS = List.of(
+            "/api/oauth2/",
+            "/api/register/",
+            "/api/login",
+            "/api/swagger-ui/",
+            "/api/v3/api-docs/",
+            "/api/csrf-token",
+            "/api/project/invite/accept"
+    );
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = request.getHeader("Authorization");
+        String token = accessToken;
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String path = httpRequest.getRequestURI();
+
+        if (EXCLUDE_URLS.stream().anyMatch(path::startsWith)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
+        if (accessToken == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if(token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
+
+        log.info("token: {}", token);
+        log.info("access: {}", accessToken);
+        log.info("role: {}", jwtUtil.getCategory(token));
+
+
+        try {
+            jwtUtil.isExpired(token);
+        } catch (ExpiredJwtException e) {
+            PrintWriter writer = response.getWriter();
+            writer.print("access token expired");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String email = jwtUtil.getEmail(token);
+
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
