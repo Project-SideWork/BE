e6662a1185380acea700029e539998722e3b3928
Gijin Park <dugudada01@naver.com>
2026-02-10 18:05:49 +0900

    feat: ProjectUserEntity 조회 쿼리메서드 구현

--- main-modules/project/application/src/main/java/com/sidework/project/application/port/out/ProjectUserOutPort.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/port/out/ProjectUserOutPort.java
@@ -5,9 +5,11 @@ import com.sidework.project.domain.ProjectRole;
 import com.sidework.project.domain.ProjectUser;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProjectUserOutPort {
     void save(ProjectUser projectUser);
     List<ProjectRole> queryUserRolesByProject(Long userId, Long projectId);
     List<Long> queryAllProjectIds(Long userId);
+	Optional<ProjectUser> findByProjectIdAndUserId(Long projectId, Long userId);
 }
