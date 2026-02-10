491044683327ed0633f1089221c589dd738f2665
Gijin Park <dugudada01@naver.com>
2026-02-10 18:04:46 +0900

    feat: ApplyStatus 업데이트 메서드 추가

--- main-modules/project/domain/src/main/java/com/sidework/project/domain/ProjectUser.java
+++ main-modules/project/domain/src/main/java/com/sidework/project/domain/ProjectUser.java
@@ -32,4 +32,8 @@ public class ProjectUser {
                 .role(role)
                 .build();
     }
+
+    public void updateStatus(ApplyStatus status) {
+        this.status = status;
+    }
 }
