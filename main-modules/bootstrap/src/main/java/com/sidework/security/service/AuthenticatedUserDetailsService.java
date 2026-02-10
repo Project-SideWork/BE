1bd81a60b6f5d678c236d0d7412e609b0ba9a950
Jinyoung <glitt5384@naver.com>
2026-02-10 20:39:53 +0900

    fix: SecurityAuthenticatedUser 미사용

--- main-modules/bootstrap/src/main/java/com/sidework/security/service/AuthenticatedUserDetailsService.java
+++ main-modules/bootstrap/src/main/java/com/sidework/security/service/AuthenticatedUserDetailsService.java
@@ -1,15 +1,11 @@
 package com.sidework.security.service;
 
-import com.sidework.common.auth.AuthenticatedUser;
-import com.sidework.common.auth.SecurityAuthenticatedUser;
 import com.sidework.security.dto.AuthenticatedUserDetails;
 import com.sidework.user.application.port.out.UserOutPort;
 import com.sidework.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,13 +14,9 @@ import org.springframework.stereotype.Service;
 public class AuthenticatedUserDetailsService implements UserDetailsService {
     private final UserOutPort repo;
     @Override
-    public AuthenticatedUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = repo.findByEmail(email);
-        AuthenticatedUser authenticatedUser = new SecurityAuthenticatedUser(
-                user.getId(), user.getEmail(), user.getName()
-        );
-
-        return new AuthenticatedUserDetails(authenticatedUser.getId(),
-                authenticatedUser.getEmail(), authenticatedUser.getName(), user.getPassword());
+    public AuthenticatedUserDetails loadUserByUsername(String email) {
+            User user = repo.findByEmail(email);
+            return new AuthenticatedUserDetails(
+                    user.getId(), user.getEmail(), user.getName(), user.getPassword());
     }
 }
