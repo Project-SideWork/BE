c818ac656c004d1adfbcd7b13f5a45f2a72d74e1
Jinyoung <glitt5384@naver.com>
2026-02-10 20:45:37 +0900

    fix: access header 노출

--- main-modules/bootstrap/src/main/java/com/sidework/security/config/SecurityConfig.java
+++ main-modules/bootstrap/src/main/java/com/sidework/security/config/SecurityConfig.java
@@ -92,6 +92,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:63342", "https://mail.naver.com", "http://180.210.81.232:8080", "https://docktalk.co.kr", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-CSRF-TOKEN"));
+        configuration.setExposedHeaders(List.of("access"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
