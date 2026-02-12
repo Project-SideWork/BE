c027034bdac07a72734626da2dbbece6b8476930
Gijin Park <dugudada01@naver.com>
2026-02-12 15:28:22 +0900

    feat: ProjectRecruitPosition 도메인/엔티티 등록

---
+++ main-modules/project/domain/src/main/java/com/sidework/project/domain/ProjectRecruitPosition.java
@@ -0,0 +1,28 @@
+package com.sidework.project.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectRecruitPosition {
+
+	private Long id;
+	private Long projectId;
+	private ProjectRole role;
+	private Integer headCount;
+	private SkillLevel level;
+
+	public static ProjectRecruitPosition create(Long projectId, ProjectRole role, Integer headCount, SkillLevel level) {
+		return ProjectRecruitPosition.builder()
+			.projectId(projectId)
+			.role(role)
+			.headCount(headCount)
+			.level(level)
+			.build();
+	}
+}
