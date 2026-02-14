a065b8c679442b606745399ebffc4dacbec26768
Jinyoung <glitt5384@naver.com>
2026-02-14 23:29:16 +0900

    feat: 채팅방 영속성-도메인 변환 매퍼

---
+++ main-modules/chat/infrastructure/src/main/java/com/sidework/chat/persistence/mapper/ChatRoomMapper.java
@@ -0,0 +1,31 @@
+package com.sidework.chat.persistence.mapper;
+
+import com.sidework.chat.persistence.entity.ChatRoomEntity;
+import com.sidework.domain.ChatRoom;
+import org.mapstruct.Mapper;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+@Mapper(componentModel = "spring")
+public interface ChatRoomMapper {
+    ZoneId PROJECT_ZONE = ZoneId.of("Asia/Seoul");
+
+    ChatRoom toDomain(ChatRoomEntity entity);
+    ChatRoomEntity toEntity(ChatRoom domain);
+
+    default Instant map(LocalDate value) {
+        if (value == null) return null;
+        return value
+                .atStartOfDay(PROJECT_ZONE)
+                .toInstant();
+    }
+
+    default LocalDate map(Instant value) {
+        if (value == null) return null;
+        return value
+                .atZone(PROJECT_ZONE)
+                .toLocalDate();
+    }
+}
