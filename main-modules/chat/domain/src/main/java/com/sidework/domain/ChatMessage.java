41f93d280c7344a62c2086fbecaca66d154f903d
Jinyoung <glitt5384@naver.com>
2026-02-14 23:27:46 +0900

    feat: 채팅 메시지 도메인 객체

---
+++ main-modules/chat/domain/src/main/java/com/sidework/domain/ChatMessage.java
@@ -0,0 +1,31 @@
+package com.sidework.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    private Long id;
+
+    private Long chatRoomId;
+
+    private Long senderId; // ChatUser의 Id가 아닌 User의 Id
+
+    private String content;
+
+    private Boolean isDeleted;
+
+    public static ChatMessage create(Long chatRoomId, Long senderId, String content) {
+        return ChatMessage.builder()
+                .chatRoomId(chatRoomId)
+                .senderId(senderId)
+                .content(content)
+                .isDeleted(false)
+                .build();
+    }
+}
