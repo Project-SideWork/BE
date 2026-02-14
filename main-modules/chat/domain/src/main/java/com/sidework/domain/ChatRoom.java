9acf1b37b0a3c58d5af40d657c285a87636c88cc
Jinyoung <glitt5384@naver.com>
2026-02-14 23:28:02 +0900

    feat: 채팅방 도메인 객체

---
+++ main-modules/chat/domain/src/main/java/com/sidework/domain/ChatRoom.java
@@ -0,0 +1,24 @@
+package com.sidework.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoom {
+    private Long id;
+
+    private String lastMessageContent;
+
+    private LocalDateTime lastMessageSentTime;
+
+    public static ChatRoom create() {
+        return new ChatRoom();
+    }
+}
