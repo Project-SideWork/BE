f0a45f63b3f6698d62671777775444b57ddb15df
Jinyoung <glitt5384@naver.com>
2026-02-16 23:46:16 +0900

    feat: 채팅방 대화내역 불러오는 API

--- main-modules/chat/application/src/main/java/com/sidework/chat/application/adapter/ChatController.java
+++ main-modules/chat/application/src/main/java/com/sidework/chat/application/adapter/ChatController.java
@@ -1,8 +1,10 @@
 package com.sidework.chat.application.adapter;
 
 import com.sidework.chat.application.port.in.ChatCommandUseCase;
-import com.sidework.chat.application.port.in.ChatContent;
+import com.sidework.chat.application.port.in.ChatMessageQueryResult;
+import com.sidework.chat.application.port.in.ChatQueryUseCase;
 import com.sidework.common.response.ApiResponse;
+import com.sidework.common.response.CursorResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +15,19 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ChatController {
     private final ChatCommandUseCase chatCommandService;
+    private final ChatQueryUseCase chatQueryService;
 
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> sendNewChat(@RequestBody ChatContent chatContent) {
         chatCommandService.processStartNewChat(chatContent);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.onSuccessCreated());
+    }
+
+    @GetMapping("/{chatRoomId}")
+    public ResponseEntity<ApiResponse<CursorResponse<ChatRecord>>> getMessages(@PathVariable("chatRoomId") Long chatRoomId,
+                                                                               @RequestParam(required = false, value = "cursor") String cursor) {
+        ChatMessageQueryResult queryRes = chatQueryService.queryByChatRoomId(chatRoomId, cursor);
+        CursorResponse<ChatRecord> res = new CursorResponse<>(queryRes.items(), queryRes.nextCursor(), queryRes.hasNext());
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(res));
     }
 }
