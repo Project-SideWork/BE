28ebd6c077dd57e2d07b93666de6bbf92bbd81c5
Jinyoung <glitt5384@naver.com>
2026-02-16 23:47:32 +0900

    feat: ChatMessage의 LDT <-> Instant

--- main-modules/chat/infrastructure/src/main/java/com/sidework/chat/persistence/mapper/ChatMessageMapper.java
+++ main-modules/chat/infrastructure/src/main/java/com/sidework/chat/persistence/mapper/ChatMessageMapper.java
@@ -4,8 +4,28 @@ import com.sidework.chat.persistence.entity.ChatMessageEntity;
 import com.sidework.domain.ChatMessage;
 import org.mapstruct.Mapper;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 @Mapper(componentModel = "spring")
 public interface ChatMessageMapper {
+    ZoneId PROJECT_ZONE = ZoneId.of("Asia/Seoul");
+
     ChatMessage toDomain(ChatMessageEntity entity);
     ChatMessageEntity toEntity(ChatMessage domain);
+
+    default Instant map(LocalDate value) {
+        if (value == null) return null;
+        return value
+                .atStartOfDay(PROJECT_ZONE)
+                .toInstant();
+    }
+
+    default LocalDate map(Instant value) {
+        if (value == null) return null;
+        return value
+                .atZone(PROJECT_ZONE)
+                .toLocalDate();
+    }
 }
