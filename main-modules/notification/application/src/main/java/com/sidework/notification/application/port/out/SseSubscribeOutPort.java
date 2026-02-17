b76848318ec1da3fda8158404ad6ad3818a4f986
Jinyoung <glitt5384@naver.com>
2026-02-17 22:49:14 +0900

    feat: common module로 sse 관련 코드 이동

--- main-modules/notification/application/src/main/java/com/sidework/notification/application/port/out/SseSubscribeOutPort.java
+++
@@ -1,7 +0,0 @@
-package com.sidework.notification.application.port.out;
-
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-public interface SseSubscribeOutPort {
-	SseEmitter subscribe(Long userId);
-}
