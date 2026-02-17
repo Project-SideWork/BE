25a4835467c45efaba3ae39b8cc2e9c75e8ea245
Jinyoung <glitt5384@naver.com>
2026-02-17 22:50:41 +0900

    feat: 기존 채팅방 채팅 API

--- main-modules/chat/application/src/main/java/com/sidework/chat/application/adapter/ChatController.java
+++ main-modules/chat/application/src/main/java/com/sidework/chat/application/adapter/ChatController.java
@@ -3,12 +3,15 @@ package com.sidework.chat.application.adapter;
 import com.sidework.chat.application.port.in.ChatCommandUseCase;
 import com.sidework.chat.application.port.in.ChatMessageQueryResult;
 import com.sidework.chat.application.port.in.ChatQueryUseCase;
+import com.sidework.common.event.sse.port.in.SseSubscribeUseCase;
 import com.sidework.common.response.ApiResponse;
 import com.sidework.common.response.CursorResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/api/v1/chats")
@@ -16,10 +19,23 @@ import org.springframework.web.bind.annotation.*;
 public class ChatController {
     private final ChatCommandUseCase chatCommandService;
     private final ChatQueryUseCase chatQueryService;
+    private final SseSubscribeUseCase sseSubscribeUseCase;
+
+    @GetMapping(value = "/subscribe/{chatRoomId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@PathVariable("chatRoomId") Long chatRoomId) {
+        return sseSubscribeUseCase.subscribeChat(chatRoomId);
+    }
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Void>> sendNewChat(@RequestBody ChatContent chatContent) {
+    public ResponseEntity<ApiResponse<Void>> sendNewChat(@RequestBody NewChatCommand chatContent) {
         chatCommandService.processStartNewChat(chatContent);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.onSuccessCreated());
+    }
+
+    @PostMapping("/{chatRoomId}")
+    public ResponseEntity<ApiResponse<Void>> sendNewChatToExistRoom(@PathVariable("chatRoomId") Long chatRoomId,
+                                                                    @RequestBody ExistChatCommand chatContent) {
+        chatCommandService.processExistChat(chatRoomId, chatContent);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.onSuccessCreated());
     }
 
