1342b9bbc4812e54edf61e2842437c04fe003a6e
Jinyoung <glitt5384@naver.com>
2026-02-17 22:51:17 +0900

    feat: common module로 sse 관련 코드 이동

--- main-modules/notification/application/src/main/java/com/sidework/notification/application/port/in/SseSubscribeUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.sidework.notification.application.port.in;
-
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-public interface SseSubscribeUseCase {
-
-	SseEmitter subscribe(Long userId);
-}
