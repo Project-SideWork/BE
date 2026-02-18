a455e5e0d53bced5c3ac57e48022f59c3401c0ea
Gijin Park <dugudada01@naver.com>
2026-02-18 19:31:35 +0900

    feat: RecruitPosition 객체 생성 메서드

--- main-modules/project/application/src/main/java/com/sidework/project/application/port/in/RecruitPosition.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/port/in/RecruitPosition.java
@@ -16,4 +16,8 @@ public record RecruitPosition(
 
         @NotNull
         SkillLevel level   // 초급 / 중급 / 고급
-) {}
+) {
+	public static RecruitPosition of(ProjectRole role, Integer headCount, SkillLevel level) {
+		return new RecruitPosition(role, headCount, level);
+	}
+}
