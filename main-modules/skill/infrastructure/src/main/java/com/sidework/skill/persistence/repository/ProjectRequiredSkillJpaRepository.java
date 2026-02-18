fe9fcae810a723fb930b07ba778e7a28d7efebb6
Gijin Park <dugudada01@naver.com>
2026-02-18 20:45:30 +0900

    chore: 미사용 import 제거

--- main-modules/skill/infrastructure/src/main/java/com/sidework/skill/persistence/repository/ProjectRequiredSkillJpaRepository.java
+++ main-modules/skill/infrastructure/src/main/java/com/sidework/skill/persistence/repository/ProjectRequiredSkillJpaRepository.java
@@ -1,6 +1,5 @@
 package com.sidework.skill.persistence.repository;
 
-import com.sidework.skill.application.port.in.ProjectRequiredQueryUseCase;
 import com.sidework.skill.persistence.entity.ProjectRequiredSkillEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
