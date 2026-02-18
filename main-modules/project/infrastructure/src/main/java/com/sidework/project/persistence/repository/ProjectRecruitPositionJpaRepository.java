cc2c80d02ef08565430acff5b74a3ad8a88cc467
Gijin Park <dugudada01@naver.com>
2026-02-18 20:32:43 +0900

    fix: @Modifying(clearAutomatically = true) 및 jpql 추가

--- main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectRecruitPositionJpaRepository.java
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectRecruitPositionJpaRepository.java
@@ -3,7 +3,9 @@ package com.sidework.project.persistence.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sidework.project.persistence.entity.ProjectRecruitPositionEntity;
@@ -13,5 +15,7 @@ public interface ProjectRecruitPositionJpaRepository extends JpaRepository<Proje
 
 	List<ProjectRecruitPositionEntity> findByProjectId(Long projectId);
 
-	void deleteByProjectId(Long projectId);
+	@Modifying(clearAutomatically = true)
+	@Query("DELETE FROM ProjectRecruitPositionEntity WHERE projectId = :projectId")
+	void deleteByProjectId(@Param("projectId") Long projectId);
 }
