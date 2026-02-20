a4d327faf828b32e8cb796a96d81f26c60eb6ea0
Jinyoung <glitt5384@naver.com>
2026-02-21 08:38:00 +0900

    fix: subscribeUser가 외부로부터 userId 받도록 수정

--- main-modules/common/src/main/java/com/sidework/common/event/sse/port/in/SseSubscribeUseCase.java
+++ main-modules/common/src/main/java/com/sidework/common/event/sse/port/in/SseSubscribeUseCase.java
@@ -3,6 +3,6 @@ package com.sidework.common.event.sse.port.in;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public interface SseSubscribeUseCase {
-	SseEmitter subscribeUser();
+	SseEmitter subscribeUser(Long userId);
 	SseEmitter subscribeChat(Long chatRoomId);
 }
