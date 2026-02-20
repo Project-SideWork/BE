5a09cc81620c2e6cb30a7aa31022d2652f046864
Jinyoung <glitt5384@naver.com>
2026-02-21 08:36:46 +0900

    chore: 함수명 변경

--- main-modules/chat/application/src/main/java/com/sidework/chat/application/port/in/ChatCommandUseCase.java
+++ main-modules/chat/application/src/main/java/com/sidework/chat/application/port/in/ChatCommandUseCase.java
@@ -4,6 +4,6 @@ import com.sidework.chat.application.adapter.ExistChatCommand;
 import com.sidework.chat.application.adapter.NewChatCommand;
 
 public interface ChatCommandUseCase {
-    void processStartNewChat(NewChatCommand chatContent);
-    void processExistChat(Long chatRoomId, ExistChatCommand chatContent);
+    void processStartNewChat(Long senderId, NewChatCommand chatContent);
+    void processResumeChat(Long chatRoomId, Long senderId, ExistChatCommand chatContent);
 }
