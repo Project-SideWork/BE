8f779fd442fc6e8280dda774bd760229a7599310
Jinyoung <glitt5384@naver.com>
2026-02-21 08:36:57 +0900

    feat: @AuthenticationPrincipal 추가

--- main-modules/chat/application/src/main/java/com/sidework/chat/application/adapter/ChatController.java
+++ main-modules/chat/application/src/main/java/com/sidework/chat/application/adapter/ChatController.java
@@ -3,6 +3,7 @@ package com.sidework.chat.application.adapter;
 import com.sidework.chat.application.port.in.ChatCommandUseCase;
 import com.sidework.chat.application.port.in.ChatMessageQueryResult;
 import com.sidework.chat.application.port.in.ChatQueryUseCase;
+import com.sidework.common.auth.AuthenticatedUserDetails;
 import com.sidework.common.event.sse.port.in.SseSubscribeUseCase;
 import com.sidework.common.response.ApiResponse;
 import com.sidework.common.response.CursorResponse;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -23,26 +25,34 @@ public class ChatController {
     private final SseSubscribeUseCase sseSubscribeUseCase;
 
     @GetMapping(value = "/subscribe/{chatRoomId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@PathVariable("chatRoomId") Long chatRoomId) {
+    public SseEmitter subscribe(
+            @AuthenticationPrincipal AuthenticatedUserDetails user,
+            @PathVariable("chatRoomId") Long chatRoomId) {
         return sseSubscribeUseCase.subscribeChat(chatRoomId);
     }
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Void>> sendNewChat(@Validated @RequestBody NewChatCommand chatContent) {
-        chatCommandService.processStartNewChat(chatContent);
+    public ResponseEntity<ApiResponse<Void>> sendNewChatToCreateRoom(
+            @AuthenticationPrincipal AuthenticatedUserDetails user,
+            @Validated @RequestBody NewChatCommand chatContent) {
+        chatCommandService.processStartNewChat(user.getId(), chatContent);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.onSuccessCreated());
     }
 
     @PostMapping("/{chatRoomId}")
-    public ResponseEntity<ApiResponse<Void>> sendNewChatToExistRoom(@PathVariable("chatRoomId") Long chatRoomId,
-                                                                    @Validated @RequestBody ExistChatCommand chatContent) {
-        chatCommandService.processExistChat(chatRoomId, chatContent);
+    public ResponseEntity<ApiResponse<Void>> sendNewChatToExistRoom(
+            @AuthenticationPrincipal AuthenticatedUserDetails user,
+            @PathVariable("chatRoomId") Long chatRoomId,
+            @Validated @RequestBody ExistChatCommand chatContent) {
+        chatCommandService.processResumeChat(chatRoomId, user.getId(), chatContent);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.onSuccessCreated());
     }
 
     @GetMapping("/{chatRoomId}")
-    public ResponseEntity<ApiResponse<CursorResponse<ChatRecord>>> getMessages(@PathVariable("chatRoomId") Long chatRoomId,
-                                                                               @RequestParam(required = false, value = "cursor") String cursor) {
+    public ResponseEntity<ApiResponse<CursorResponse<ChatRecord>>> getMessages(
+            @AuthenticationPrincipal AuthenticatedUserDetails user,
+            @PathVariable("chatRoomId") Long chatRoomId,
+            @RequestParam(required = false, value = "cursor") String cursor) {
         ChatMessageQueryResult queryRes = chatQueryService.queryMessagesByChatRoomId(chatRoomId, cursor);
         CursorResponse<ChatRecord> res = new CursorResponse<>(queryRes.items(), queryRes.nextCursor(), queryRes.hasNext());
         return ResponseEntity.ok().body(ApiResponse.onSuccess(res));
