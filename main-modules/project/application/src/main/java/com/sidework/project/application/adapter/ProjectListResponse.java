54013f8e32f346da85bca13d86af9ca1d8dd7d75
Gijin Park <dugudada01@naver.com>
2026-02-26 20:22:29 +0900

    feat: 프로젝트 리스트 res

--- main-modules/project/application/src/main/java/com/sidework/project/application/adapter/ProjectListResponse.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/adapter/ProjectListResponse.java
@@ -16,4 +16,27 @@ public record ProjectListResponse(
         String creatorName,
         int durationMonths
 ) {
+    public static ProjectListResponse of(
+            Long projectId,
+            String title,
+            String description,
+            ProjectStatus status,
+            Integer remainingDays,
+            boolean liked,
+            List<ProjectDetailResponse.RecruitPositionResponse> recruitPositions,
+            List<String> requiredStacks,
+            String creatorName,
+            int durationMonths) {
+        return new ProjectListResponse(
+                projectId,
+                title,
+                description,
+                status,
+                remainingDays,
+                liked,
+                recruitPositions,
+                requiredStacks,
+                creatorName,
+                durationMonths);
+    }
 }
