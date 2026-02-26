89d4e015055675c0a6100f87605047d3c7eb19b7
Gijin Park <dugudada01@naver.com>
2026-02-26 20:23:57 +0900

    feat: ProjectUserEntity owner 유저 조회

--- main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectUserJpaRepository.java
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectUserJpaRepository.java
@@ -35,4 +35,6 @@ public interface ProjectUserJpaRepository extends JpaRepository<ProjectUserEntit
     List<ProjectUserEntity> findAllByProjectId(Long projectId);
 
     List<ProjectUserEntity> findByUserIdAndProjectIdIn(Long userId, List<Long> projectIds);
+
+    List<ProjectUserEntity> findByProjectIdInAndRole(List<Long> projectIds, ProjectRole role);
 }
