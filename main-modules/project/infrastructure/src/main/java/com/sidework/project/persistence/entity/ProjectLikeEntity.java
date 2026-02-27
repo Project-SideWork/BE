10e886d59f95a6859ab46757bac047e554c44a8b
Gijin Park <dugudada01@naver.com>
2026-02-27 16:10:06 +0900

    feat: 프로젝트 좋아요 엔티티

---
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/entity/ProjectLikeEntity.java
@@ -0,0 +1,33 @@
+package com.sidework.project.persistence.entity;
+
+import com.sidework.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "project_likes")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectLikeEntity extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "project_id", nullable = false)
+	private Long projectId;
+
+	@Column(name = "user_id", nullable = false)
+	private Long userId;
+
+}
