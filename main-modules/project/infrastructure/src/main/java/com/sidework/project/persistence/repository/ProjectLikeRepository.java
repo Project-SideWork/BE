01122021fc923b73b5ccc5d6c9d2964ef2623f2a
Gijin Park <dugudada01@naver.com>
2026-03-01 14:26:34 +0900

    feat: ProjectLike 조회 레포

--- main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectLikeRepository.java
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectLikeRepository.java
@@ -1,6 +1,10 @@
 package com.sidework.project.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sidework.project.persistence.entity.ProjectLikeEntity;
@@ -9,4 +13,8 @@ import com.sidework.project.persistence.entity.ProjectLikeEntity;
 public interface ProjectLikeRepository extends JpaRepository<ProjectLikeEntity, Long> {
 	boolean existsByUserIdAndProjectId(Long userId, Long projectId);
 	void deleteByUserIdAndProjectId(Long userId, Long projectId);
+
+	@Query("SELECT pl.projectId FROM ProjectLikeEntity pl WHERE pl.userId = :userId AND pl.projectId IN :projectIds")
+	List<Long> findProjectIdsByUserIdAndProjectIdIn(@Param("userId") Long userId, @Param("projectIds") List<Long> projectIds);
+
 }
