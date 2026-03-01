d70d24d78131e62e899c59857439d7f07eb0b895
Gijin Park <dugudada01@naver.com>
2026-03-01 21:02:59 +0900

    feat: @Modifying 어노테이션 추가

--- main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectLikeRepository.java
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectLikeRepository.java
@@ -3,6 +3,7 @@ package com.sidework.project.persistence.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,8 @@ import com.sidework.project.persistence.entity.ProjectLikeEntity;
 @Repository
 public interface ProjectLikeRepository extends JpaRepository<ProjectLikeEntity, Long> {
 	boolean existsByUserIdAndProjectId(Long userId, Long projectId);
+
+	@Modifying
 	void deleteByUserIdAndProjectId(Long userId, Long projectId);
 
 	@Query("SELECT pl.projectId FROM ProjectLikeEntity pl WHERE pl.userId = :userId AND pl.projectId IN :projectIds")
