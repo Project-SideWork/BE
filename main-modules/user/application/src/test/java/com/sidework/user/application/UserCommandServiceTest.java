f7eeb89eac9498babc7204c21a2f7e441631a06e
Jinyoung <glitt5384@naver.com>
2026-03-08 02:43:32 +0900

    feat: 닉네임, 전화번호 중복 테스트 코드

--- main-modules/user/application/src/test/java/com/sidework/user/application/UserCommandServiceTest.java
+++ main-modules/user/application/src/test/java/com/sidework/user/application/UserCommandServiceTest.java
@@ -1,7 +1,9 @@
 package com.sidework.user.application;
 
+import com.sidework.region.application.exception.InvalidRegionLevelException;
 import com.sidework.region.application.exception.RegionNotFoundException;
 import com.sidework.region.application.port.out.RegionOutPort;
+import com.sidework.user.application.exception.DuplicatedInformationException;
 import com.sidework.user.application.port.in.SignUpCommand;
 import com.sidework.user.application.port.out.UserOutPort;
 import com.sidework.user.application.service.UserCommandService;
@@ -38,6 +40,11 @@ public class UserCommandServiceTest {
     void 정상적인_회원가입_요청_DTO로_회원가입에_성공한다() {
         SignUpCommand command = createCommand();
         when(regionRepo.existsById(command.residenceRegionId())).thenReturn(true);
+        when(regionRepo.checkIsSubRegion(command.residenceRegionId())).thenReturn(true);
+        when(repo.existsByEmail(command.email())).thenReturn(false);
+        when(repo.existsByNickname(command.nickname())).thenReturn(false);
+        when(repo.existsByTel(command.tel())).thenReturn(false);
+
         service.signUp(command);
 
         // then
@@ -56,6 +63,56 @@ public class UserCommandServiceTest {
         when(regionRepo.existsById(command.residenceRegionId())).thenReturn(false);
         assertThrows(
                 RegionNotFoundException.class,
+                () -> service.signUp(command)
+        );
+    }
+
+    @Test
+    void 회원가입_요청_DTO에_포함된_거주지역ID가_하위행정구역이_아니면_InvalidRegionLevelException을_던진다() {
+        SignUpCommand command = createCommand();
+        when(regionRepo.existsById(command.residenceRegionId())).thenReturn(true);
+        when(regionRepo.checkIsSubRegion(command.residenceRegionId())).thenReturn(false);
+        assertThrows(
+                InvalidRegionLevelException.class,
+                () -> service.signUp(command)
+        );
+    }
+
+    @Test
+    void 회원가입_요청_DTO에_포함된_이메일이_중복되면_DuplicatedInformationException을_던진다() {
+        SignUpCommand command = createCommand();
+        when(regionRepo.existsById(command.residenceRegionId())).thenReturn(true);
+        when(regionRepo.checkIsSubRegion(command.residenceRegionId())).thenReturn(true);
+        when(repo.existsByEmail(command.email())).thenReturn(true);
+        assertThrows(
+                DuplicatedInformationException.class,
+                () -> service.signUp(command)
+        );
+    }
+
+    @Test
+    void 회원가입_요청_DTO에_포함된_닉네임이_중복되면_DuplicatedInformationException을_던진다() {
+        SignUpCommand command = createCommand();
+        when(regionRepo.existsById(command.residenceRegionId())).thenReturn(true);
+        when(regionRepo.checkIsSubRegion(command.residenceRegionId())).thenReturn(true);
+        when(repo.existsByEmail(command.email())).thenReturn(false);
+        when(repo.existsByNickname(command.nickname())).thenReturn(true);
+        assertThrows(
+                DuplicatedInformationException.class,
+                () -> service.signUp(command)
+        );
+    }
+
+    @Test
+    void 회원가입_요청_DTO에_포함된_전화번호가_중복되면_DuplicatedInformationException을_던진다() {
+        SignUpCommand command = createCommand();
+        when(regionRepo.existsById(command.residenceRegionId())).thenReturn(true);
+        when(regionRepo.checkIsSubRegion(command.residenceRegionId())).thenReturn(true);
+        when(repo.existsByEmail(command.email())).thenReturn(false);
+        when(repo.existsByNickname(command.nickname())).thenReturn(false);
+        when(repo.existsByTel(command.tel())).thenReturn(true);
+        assertThrows(
+                DuplicatedInformationException.class,
                 () -> service.signUp(command)
         );
     }
