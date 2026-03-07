d3b1de0b535bb1357e525d1c1584f2128755131b
Jinyoung <glitt5384@naver.com>
2026-03-08 02:43:59 +0900

    feat: 나이 최대/최소 제한 추가

--- main-modules/user/application/src/main/java/com/sidework/user/application/port/in/SignUpCommand.java
+++ main-modules/user/application/src/main/java/com/sidework/user/application/port/in/SignUpCommand.java
@@ -7,7 +7,10 @@ public record SignUpCommand(
         @NotNull @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.") String password,
         @NotNull String name,
         @NotNull String nickname,
-        @NotNull Integer age,
+        @NotNull
+        @Min(value = 19, message = "나이는 19살 이상이어야 합니다.")
+        @Max(value = 100, message = "나이는 100살을 초과할 수 없습니다.")
+        Integer age,
         @NotNull String tel,
         @NotNull @Positive Long residenceRegionId
 ) {
