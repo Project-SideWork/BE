ff832911b31f78d6c0bcc925480439f038628633
Jinyoung <glitt5384@naver.com>
2026-03-08 01:10:02 +0900

    feat: 회원가입 시 전달받은 지역 ID가 DB에 존재하는지 확인

--- main-modules/user/application/src/main/java/com/sidework/user/application/service/UserCommandService.java
+++ main-modules/user/application/src/main/java/com/sidework/user/application/service/UserCommandService.java
@@ -1,7 +1,8 @@
 package com.sidework.user.application.service;
 
+import com.sidework.region.application.exception.RegionNotFoundException;
+import com.sidework.region.application.port.out.RegionOutPort;
 import com.sidework.user.application.port.in.SignUpCommand;
-import com.sidework.user.application.exception.InvalidCommandException;
 import com.sidework.user.application.port.in.UserCommandUseCase;
 import com.sidework.user.application.port.out.UserOutPort;
 import com.sidework.user.domain.User;
@@ -15,14 +16,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = false)
 public class UserCommandService implements UserCommandUseCase {
+    private final RegionOutPort regionRepository;
     private final UserOutPort userRepository;
     private final PasswordEncoder encoder;
 
     @Override
     public void signUp(SignUpCommand command) {
         User user = User.create(command.email(), command.name(), command.nickname(), encodePassword(command.password())
-                , command.age(), command.tel(), UserType.LOCAL);
-        userRepository.save(user);
+                , command.age(), command.tel(), command.residenceRegionId(), UserType.LOCAL);
+        if(regionRepository.existsById(command.residenceRegionId())) {
+            userRepository.save(user);
+        } else {
+            throw new RegionNotFoundException(command.residenceRegionId());
+        }
     }
 
     private String encodePassword(String rawPassword) {
