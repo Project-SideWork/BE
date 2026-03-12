5e1c959f023ee6818e58cac2f904103e1e37d508
Jinyoung <glitt5384@naver.com>
2026-03-12 23:43:01 +0900

    feat: 깃허브 OAuth API 요청에 인증 정보 추가

--- main-modules/bootstrap/src/main/java/com/sidework/security/controller/AuthController.java
+++ main-modules/bootstrap/src/main/java/com/sidework/security/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.sidework.security.controller;
 
+import com.sidework.common.auth.AuthenticatedUserDetails;
 import com.sidework.security.dto.LoginCommand;
 import com.sidework.security.docs.AuthControllerDocs;
 import com.sidework.security.util.CookieUtil;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -30,7 +32,9 @@ public class AuthController implements AuthControllerDocs {
     }
 
     @GetMapping("/login/github")
-    public void linkGithub(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void linkGithub(
+            @AuthenticationPrincipal AuthenticatedUserDetails user,
+            HttpServletRequest request, HttpServletResponse response) throws IOException {
         String token = CookieUtil.getCookie(request, "access")
                 .map(Cookie::getValue)
                 .orElseThrow();
