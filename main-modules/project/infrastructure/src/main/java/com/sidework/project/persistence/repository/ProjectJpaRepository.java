d37ddcdddff9b54ee979d421950eb9a1dcf16496
Gijin Park <dugudada01@naver.com>
2026-03-12 15:13:38 +0900

    feat: 누락 코드 추가

--- main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectJpaRepository.java
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/repository/ProjectJpaRepository.java
@@ -2,6 +2,8 @@ package com.sidework.project.persistence.repository;
 
 import com.sidework.project.application.dto.ProjectTitleDto;
 import com.sidework.project.persistence.entity.ProjectEntity;
+import com.sidework.project.persistence.repository.custom.CustomProjectJpaRepository;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,24 +14,24 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProjectJpaRepository extends JpaRepository<ProjectEntity, Long> {
-    @Query(
-            """
-            SELECT new com.sidework.project.application.dto.ProjectTitleDto(p.id, p.title) FROM ProjectEntity p
-            WHERE p.id IN (:ids)
-            """
-    )
-    List<ProjectTitleDto> findProjectionsByIds(@Param("ids") List<Long> ids);
+public interface ProjectJpaRepository extends JpaRepository<ProjectEntity, Long>, CustomProjectJpaRepository {
+	@Query(
+		"""
+		SELECT new com.sidework.project.application.dto.ProjectTitleDto(p.id, p.title) FROM ProjectEntity p
+		WHERE p.id IN (:ids)
+		"""
+	)
+	List<ProjectTitleDto> findProjectionsByIds(@Param("ids") List<Long> ids);
 
-    @Query(
-        """
-        SELECT p
-        FROM ProjectEntity p
-        WHERE
-            (:keyword IS NULL OR :keyword = '')
-            OR LOWER(p.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
-            OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%'))
-        """
-    )
-    Page<ProjectEntity> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
+	@Query(
+		"""
+		SELECT p
+		FROM ProjectEntity p
+		WHERE
+			(:keyword IS NULL OR :keyword = '')
+			OR LOWER(p.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
+			OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%'))
+		"""
+	)
+	Page<ProjectEntity> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
