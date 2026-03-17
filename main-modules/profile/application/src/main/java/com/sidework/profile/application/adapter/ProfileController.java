97f664bf942a344b08b08cef42532d02e32ed899
Gijin Park <dugudada01@naver.com>
2026-03-17 20:21:35 +0900

    feat: 프로필 리스트 조회 컨트롤러 구현

--- main-modules/profile/application/src/main/java/com/sidework/profile/application/adapter/ProfileController.java
+++ main-modules/profile/application/src/main/java/com/sidework/profile/application/adapter/ProfileController.java
@@ -1,5 +1,9 @@
 package com.sidework.profile.application.adapter;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -7,10 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sidework.common.auth.AuthenticatedUserDetails;
 import com.sidework.common.response.ApiResponse;
+import com.sidework.common.response.PageResponse;
 import com.sidework.profile.application.docs.ProfileControllerDocs;
 import com.sidework.profile.application.port.in.ProfileCommandUseCase;
 import com.sidework.profile.application.port.in.ProfileQueryUseCase;
@@ -24,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 public class ProfileController implements ProfileControllerDocs {
 	private final ProfileQueryUseCase queryService;
 	private final ProfileCommandUseCase commandService;
+	private final ProfileQueryUseCase profileQueryUseCase;
 
 	@GetMapping
 	public ResponseEntity<ApiResponse<UserProfileResponse>> getUserProfile(
@@ -37,5 +44,14 @@ public class ProfileController implements ProfileControllerDocs {
 		@RequestBody ProfileUpdateCommand profileUpdateCommand) {
 		commandService.update(user.getId(), profileUpdateCommand);
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.onSuccessVoid());
+	}
+
+	@GetMapping("/list")
+	public ResponseEntity<ApiResponse<PageResponse<List<UserProfileListResponse>>>> getUserProfiles(
+		@PageableDefault(size = 20) Pageable pageable,
+		@RequestParam(name = "keyword", required = false, defaultValue = "") String keyword){
+
+		return ResponseEntity.ok(ApiResponse.onSuccess(profileQueryUseCase.getUserProfileList(keyword,pageable)));
+
 	}
 }
