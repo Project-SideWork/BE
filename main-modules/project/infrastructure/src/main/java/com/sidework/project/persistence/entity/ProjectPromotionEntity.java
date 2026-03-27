d7276a790bbcf99628a01f4f8ed432bae8a5d094
Gijin Park <dugudada01@naver.com>
2026-03-27 21:01:39 +0900

    feat: 프로젝트 홍보글 엔티티

---
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/entity/ProjectPromotionEntity.java
@@ -0,0 +1,30 @@
+package com.sidework.project.persistence.entity;
+
+import com.sidework.common.entity.BaseEntity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "project_promotions")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectPromotionEntity extends BaseEntity {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+	@Column(name = "project_id", nullable = false)
+	private Long projectId;
+
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String description;
+
+	@Column(columnDefinition = "TEXT")
+	private String demoUrl;
+}
