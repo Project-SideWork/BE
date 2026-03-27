94520e37c4a628355d4b443abbbe46076c4376af
Gijin Park <dugudada01@naver.com>
2026-03-27 20:21:17 +0900

    feat: 프로젝트 홍보글 작성 req

---
+++ main-modules/project/application/src/main/java/com/sidework/project/application/dto/ProjectPromotionCommand.java
@@ -0,0 +1,15 @@
+package com.sidework.project.application.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public record ProjectPromotionCommand(
+	@NotEmpty
+	String description,
+
+	@NotEmpty
+	List<Long> usedSkillIds,
+
+	String demoURL
+) {}
