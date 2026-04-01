1ac0db255c8679ceab9d246784af68af25065894
Jinyoung <glitt5384@naver.com>
2026-04-01 20:41:00 +0900

    feat: payment 모듈 스캔

--- main-modules/bootstrap/src/main/java/com/sidework/SideWorkApplication.java
+++ main-modules/bootstrap/src/main/java/com/sidework/SideWorkApplication.java
@@ -1,8 +1,10 @@
 package com.sidework;
 
+import com.sidework.payment.application.config.PortOneSecretProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,7 +16,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EntityScan(basePackages = {"com.sidework.project.persistence.entity", "com.sidework.user.persistence.entity",
         "com.sidework.profile.persistence.entity", "com.sidework.skill.persistence.entity",
         "com.sidework.notification.persistence.entity", "com.sidework.chat.persistence.entity",
-        "com.sidework.region.persistence.entity", "com.sidework.school.persistence.entity"})
+        "com.sidework.region.persistence.entity", "com.sidework.school.persistence.entity",
+        "com.sidework.payment.persistence.entity" })
+@EnableConfigurationProperties(PortOneSecretProperties.class)
 public class SideWorkApplication {
 
 	public static void main(String[] args) {
