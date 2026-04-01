695009d14322892205a397f251fcd80cddf511ad
Gijin Park <dugudada01@naver.com>
2026-04-01 14:35:32 +0900

    refactor: 예외처리 수정

--- main-modules/project/application/src/main/java/com/sidework/project/application/exception/ProjectPromotionNotFoundException.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/exception/ProjectPromotionNotFoundException.java
@@ -1,11 +1,10 @@
 package com.sidework.project.application.exception;
 
 import com.sidework.common.response.exception.GlobalException;
-import com.sidework.common.response.status.BaseStatusCode;
 import com.sidework.common.response.status.ErrorStatus;
 
 public class ProjectPromotionNotFoundException extends GlobalException {
-	public ProjectPromotionNotFoundException(Long promotionId, Long userId) {
-		super(ErrorStatus.PROJECT_PROMOTION_NOT_FOUND.withDetail("유저아이디: "+ userId + "홍보글 아이디: "+ promotionId + "인 프로젝트 홍보글을 찾을 수 없습니다."));
+	public ProjectPromotionNotFoundException(Long promotionId) {
+		super(ErrorStatus.PROJECT_PROMOTION_NOT_FOUND.withDetail("홍보글 아이디: "+ promotionId + "인 프로젝트 홍보글을 찾을 수 없습니다."));
 	}
 }
