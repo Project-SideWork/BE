62502ea9efc417145d18a02fae0f111beb3cba15
Jinyoung <glitt5384@naver.com>
2026-04-02 22:47:35 +0900

    feat: Item record로 변경

--- main-modules/payment/application/src/main/java/com/sidework/payment/application/port/in/Item.java
+++ main-modules/payment/application/src/main/java/com/sidework/payment/application/port/in/Item.java
@@ -1,13 +1,4 @@
 package com.sidework.payment.application.port.in;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
-public class Item {
-    private final String id;
-    private final String name;
-    private final int price;
-    private final String currency;
+public record Item(String id, String name, int price, String currency) {
 }
