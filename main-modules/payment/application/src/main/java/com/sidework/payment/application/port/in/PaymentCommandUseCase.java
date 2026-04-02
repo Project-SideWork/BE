e7ac57d4d4500b5c011e38a997d12bd9225fa759
Jinyoung <glitt5384@naver.com>
2026-04-02 22:47:19 +0900

    feat: 컨트롤러 내 비즈니스 로직 서비스로 이전

--- main-modules/payment/application/src/main/java/com/sidework/payment/application/port/in/PaymentCommandUseCase.java
+++ main-modules/payment/application/src/main/java/com/sidework/payment/application/port/in/PaymentCommandUseCase.java
@@ -2,6 +2,9 @@ package com.sidework.payment.application.port.in;
 
 import com.sidework.payment.domain.Payment;
 
+import java.util.concurrent.CompletableFuture;
+
 public interface PaymentCommandUseCase {
     void create(Payment payment);
+    CompletableFuture<Payment> syncPayment(String paymentId);
 }
