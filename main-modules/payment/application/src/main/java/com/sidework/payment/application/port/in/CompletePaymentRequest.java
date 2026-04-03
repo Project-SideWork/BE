12376db28c6b7b7b7737be10204b1c6631f288f4
Jinyoung <glitt5384@naver.com>
2026-04-03 21:04:16 +0900

    fix: record 타입으로 변경

--- main-modules/payment/application/src/main/java/com/sidework/payment/application/port/in/CompletePaymentRequest.java
+++ main-modules/payment/application/src/main/java/com/sidework/payment/application/port/in/CompletePaymentRequest.java
@@ -1,10 +1,4 @@
 package com.sidework.payment.application.port.in;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class CompletePaymentRequest {
-    private String paymentId;
+public record CompletePaymentRequest(String paymentId) {
 }
