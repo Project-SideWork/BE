73ba391b50e2019ffdd9d607343adae736b29d34
Jinyoung <glitt5384@naver.com>
2026-04-03 21:08:54 +0900

    feat: /api/v1/payments 인증/인가 제외

--- main-modules/bootstrap/src/main/java/com/sidework/security/filter/JwtFilter.java
+++ main-modules/bootstrap/src/main/java/com/sidework/security/filter/JwtFilter.java
@@ -44,7 +44,7 @@ public class JwtFilter extends OncePerRequestFilter {
             "/login/oauth2/code/github",
             "/oauth2/authorization/github",
             "/api/v1/users/email",
-            "/api/v1/payments/complete"
+            "/api/v1/payments/**"
     );
 
     private static final String TOKEN_REISSUE_API = "/api/v1/reissue";
