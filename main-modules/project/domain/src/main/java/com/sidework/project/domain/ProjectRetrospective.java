97447a2e667d7c7d81bcc6b9626d8c766e37f6b7
Gijin Park <dugudada01@naver.com>
2026-04-07 21:10:39 +0900

    feat: 프로젝트 회고 도메인

---
+++ main-modules/project/domain/src/main/java/com/sidework/project/domain/ProjectRetrospective.java
@@ -0,0 +1,31 @@
+package com.sidework.project.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectRetrospective {
+
+	private Long id;
+	private Long projectId;
+	private Long userId;
+	private String role;
+	private String strengths;
+	private String improvements;
+
+	public static  ProjectRetrospective create(Long projectId, Long userId, String role, String strengths, String improvements) {
+		return ProjectRetrospective.builder()
+			.projectId(projectId)
+			.userId(userId)
+			.role(role)
+			.strengths(strengths)
+			.improvements(improvements)
+			.build();
+	}
+
+}
