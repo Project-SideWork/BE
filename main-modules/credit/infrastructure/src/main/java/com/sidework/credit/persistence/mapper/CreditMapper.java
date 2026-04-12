30f47d3ad3d57e27cedb9a0eeb7168a1c6a6cf1b
Jinyoung <glitt5384@naver.com>
2026-04-12 19:34:09 +0900

    feat: Instant와 LDT 간 변환

--- main-modules/credit/infrastructure/src/main/java/com/sidework/credit/persistence/mapper/CreditMapper.java
+++ main-modules/credit/infrastructure/src/main/java/com/sidework/credit/persistence/mapper/CreditMapper.java
@@ -4,8 +4,28 @@ import com.sidework.credit.domain.Credit;
 import com.sidework.credit.persistence.entity.CreditEntity;
 import org.mapstruct.Mapper;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 @Mapper(componentModel = "spring")
 public interface CreditMapper {
     Credit toDomain(CreditEntity entity);
     CreditEntity toEntity(Credit domain);
+
+    ZoneId PROJECT_ZONE = ZoneId.of("Asia/Seoul");
+
+    default Instant map(LocalDate value) {
+        if (value == null) return null;
+        return value
+                .atStartOfDay(PROJECT_ZONE)
+                .toInstant();
+    }
+
+    default LocalDate map(Instant value) {
+        if (value == null) return null;
+        return value
+                .atZone(PROJECT_ZONE)
+                .toLocalDate();
+    }
 }
