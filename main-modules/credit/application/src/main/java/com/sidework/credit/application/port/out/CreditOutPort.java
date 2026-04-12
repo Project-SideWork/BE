bd4d5714d6970cc81898fd11e6ebeac9164d05a4
Jinyoung <glitt5384@naver.com>
2026-04-12 19:32:25 +0900

    feat: 사용 가능 크레딧 조회

--- main-modules/credit/application/src/main/java/com/sidework/credit/application/port/out/CreditOutPort.java
+++ main-modules/credit/application/src/main/java/com/sidework/credit/application/port/out/CreditOutPort.java
@@ -1,5 +1,11 @@
 package com.sidework.credit.application.port.out;
 
+import com.sidework.credit.domain.Credit;
+
+import java.util.List;
+
 public interface CreditOutPort {
+    void saveAll(List<Credit> domains);
     Long findAmountByUser(Long userId);
+    List<Credit> findAvailableCredits(Long userId);
 }
--- main-modules/credit/infrastructure/src/main/java/com/sidework/credit/persistence/repository/CreditJpaRepository.java
+++ main-modules/credit/infrastructure/src/main/java/com/sidework/credit/persistence/repository/CreditJpaRepository.java
@@ -6,13 +6,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CreditJpaRepository extends JpaRepository<CreditEntity, Long> {
 
     @Query("""
-            SELECT COALESCE(SUM(c.amount), 0) FROM CreditEntity c
+            SELECT COALESCE(SUM(c.remainingAmount), 0) FROM CreditEntity c
             WHERE c.userId = :userId
             """)
     Long findSumAmountByUserId(@Param("userId") Long userId);
 
+    @Query("""
+        SELECT c FROM CreditEntity c
+        WHERE c.userId = :userId
+        AND c.remainingAmount > 0
+        AND (c.expiresAt IS NULL OR c.expiresAt > CURRENT_TIMESTAMP)
+        ORDER BY c.expiresAt ASC
+        """)
+    List<CreditEntity> findAvailableCredits(@Param("userId") Long userId);
 }
