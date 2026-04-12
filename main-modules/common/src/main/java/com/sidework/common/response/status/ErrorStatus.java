7784dcfb2beed0709afb8cb3b36487833c0cb2e9
Gijin Park <dugudada01@naver.com>
2026-04-12 20:32:17 +0900

    feat: 프로젝트 회고 ErrorStatus

--- main-modules/common/src/main/java/com/sidework/common/response/status/ErrorStatus.java
+++ main-modules/common/src/main/java/com/sidework/common/response/status/ErrorStatus.java
@@ -51,6 +51,10 @@ public enum ErrorStatus implements BaseStatusCode {
     PROJECT_PROMOTION_ALREADY_EXISTS(HttpStatus.CONFLICT, "PROJECT_PROMOTION_001", "이미 최근에 홍보된 프로젝트입니다."),
     PROJECT_PROMOTION_NOT_FOUND(HttpStatus.NOT_FOUND,"PROJECT_PROMOTION_002", "해당 프로젝트 홍보글을 찾을 수 없습니다." ),
 
+    // PROJECT_RETROSPECTIVE
+    PROJECT_RETROSPECTIVE_NOT_FOUND(HttpStatus.NOT_FOUND,"PROJECT_RETROSPECTIVE_001", "해당 프로젝트 회고를 찾을 수 없습니다." ),
+    PROJECT_RETROSPECTIVE_ALREADY_EXISTS(HttpStatus.CONFLICT, "PROJECT_RETROSPECTIVE_002", "이미 최근에 작성된 프로젝트 회고입니다."),
+
     // PROFILE
     PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "PROFILE_001", "해당 프로필을 찾을 수 없습니다."),
     PROFILE_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "PROFILE_002", "이미 프로필이 존재합니다."),
