6cccf34c90214305866029ae6aac1197c4d0e573
Jinyoung <glitt5384@naver.com>
2026-04-12 19:32:57 +0900

    feat: 결제 완료 이벤트 및 리스너

---
+++ main-modules/credit/application/src/main/java/com/sidework/credit/application/event/CreditEventListener.java
@@ -0,0 +1,40 @@
+package com.sidework.credit.application.event;
+
+import com.sidework.common.event.PaymentCompleteEvent;
+import com.sidework.credit.application.exception.InsufficientCreditException;
+import com.sidework.credit.application.port.out.CreditOutPort;
+import com.sidework.credit.domain.Credit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CreditEventListener {
+    private final CreditOutPort repo;
+
+    @EventListener
+    @Transactional
+    public void onPaymentCompleted(PaymentCompleteEvent event) {
+        List<Credit> available = repo.findAvailableCredits(event.userId());
+        int spent = event.usedCredit();
+
+        for (Credit credit : available) {
+            if (spent <= 0) break;
+
+            int deduct = Math.min(credit.getAmount(), spent);
+
+            credit.spend(deduct);
+            spent -= deduct;
+        }
+
+        if(spent > 0) throw new InsufficientCreditException();
+        repo.saveAll(available);
+    }
+}
