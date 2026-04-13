c18cfd1c9fc400d206d3db7644439e46ac9123a5
Gijin Park <dugudada01@naver.com>
2026-04-13 20:37:12 +0900

    refactor: NotBlank로 변경

--- main-modules/project/application/src/main/java/com/sidework/project/application/port/in/ProjectRetrospectiveCommand.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/port/in/ProjectRetrospectiveCommand.java
@@ -1,10 +1,11 @@
 package com.sidework.project.application.port.in;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
 public record ProjectRetrospectiveCommand(
-	@NotEmpty String roleDescription,
-	@NotEmpty String strengths,
-	@NotEmpty String regrets,
-	@NotEmpty String learnings
+	@NotBlank String roleDescription,
+	@NotBlank String strengths,
+	@NotBlank String regrets,
+	@NotBlank String learnings
 ) {}
