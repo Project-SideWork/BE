983f2cf9059fe2e5b09251037d2f125562b458e5
Jinyoung <glitt5384@naver.com>
2026-04-14 22:30:26 +0900

    fix: /complete 응답 형식 수정

--- main-modules/payment/application/src/main/java/com/sidework/payment/application/adapter/PaymentController.java
+++ main-modules/payment/application/src/main/java/com/sidework/payment/application/adapter/PaymentController.java
@@ -1,6 +1,7 @@
 package com.sidework.payment.application.adapter;
 
 import com.sidework.common.auth.AuthenticatedUserDetails;
+import com.sidework.common.response.ApiResponse;
 import com.sidework.payment.application.exception.SyncPaymentException;
 import com.sidework.payment.application.port.in.CompletePaymentRequest;
 import com.sidework.payment.application.port.in.Item;
@@ -10,6 +11,7 @@ import io.portone.sdk.server.webhook.WebhookTransaction;
 import io.portone.sdk.server.webhook.WebhookVerifier;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,11 +33,12 @@ public class PaymentController {
     }
 
     @PostMapping("/complete")
-    public void completePayment(
+    public ResponseEntity<ApiResponse<Void>> completePayment(
             @AuthenticationPrincipal AuthenticatedUserDetails details,
             @RequestBody CompletePaymentRequest completeRequest) {
         paymentCommandService.syncPayment(completeRequest.paymentId()).join();
         paymentCommandService.processAfterPaymentCompleted(details.getId(), completeRequest.paymentId());
+        return ResponseEntity.ok(ApiResponse.onSuccessVoid());
     }
 
     @PostMapping("/webhook")
