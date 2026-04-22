a1011f801762c2bd6e2686b0d45bfd3596735cbd
Jinyoung <glitt5384@naver.com>
2026-04-22 14:36:40 +0900

    feat: requestparam 인터페이스로 이동

--- main-modules/profile/application/src/main/java/com/sidework/profile/application/adapter/ProfileController.java
+++ main-modules/profile/application/src/main/java/com/sidework/profile/application/adapter/ProfileController.java
@@ -49,10 +49,7 @@ public class ProfileController implements ProfileControllerDocs {
     @GetMapping("/me/projects")
 	public ResponseEntity<ApiResponse<PageResponse<List<UserProjectDto>>>> getUserProfileProject(
 		@AuthenticationPrincipal AuthenticatedUserDetails user,
-        @Min(value = 1, message = "page는 1보다 작을 수 없습니다.")
         @RequestParam(name = "page", defaultValue = "1") Integer page,
-        @Min(value = 1, message = "size는 1보다 작을 수 없습니다.")
-        @Max(value = 100, message = "size는 100보다 클 수 없습니다.")
         @RequestParam(name = "size", defaultValue = "5") Integer size) {
         Pageable pageable = PageRequest.of(page - 1, size);
 		return ResponseEntity.ok(ApiResponse.onSuccess(profileQueryUseCase.getUserProjectList(user.getId(), pageable)));
--- main-modules/profile/application/src/main/java/com/sidework/profile/application/docs/ProfileControllerDocs.java
+++ main-modules/profile/application/src/main/java/com/sidework/profile/application/docs/ProfileControllerDocs.java
@@ -8,6 +8,7 @@ import com.sidework.profile.application.adapter.UserProfileResponse;
 import com.sidework.profile.application.adapter.UserProjectDto;
 import com.sidework.profile.application.port.in.ProfileUpdateCommand;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -89,8 +90,10 @@ public interface ProfileControllerDocs {
     })
     ResponseEntity<ApiResponse<PageResponse<List<UserProjectDto>>>> getUserProfileProject(
             @AuthenticationPrincipal AuthenticatedUserDetails user,
+            @Parameter(description = "페이지 번호")
             @Min(value = 1, message = "page는 1보다 작을 수 없습니다.")
             @RequestParam(name = "page", defaultValue = "1") Integer page,
+            @Parameter(description = "페이지 크기")
             @Min(value = 1, message = "size는 1보다 작을 수 없습니다.")
             @Max(value = 100, message = "size는 100보다 클 수 없습니다.")
             @RequestParam(name = "size", defaultValue = "5") Integer size
