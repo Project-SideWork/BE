e91ffdfcc126966688a75afc634990a341e66085
Jinyoung <glitt5384@naver.com>
2026-04-22 16:58:09 +0900

    feat: 프로젝트 좋아요 취소 로직 구현

--- main-modules/project/application/src/main/java/com/sidework/project/application/port/in/ProjectLikeCommandUseCase.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/port/in/ProjectLikeCommandUseCase.java
@@ -2,4 +2,5 @@ package com.sidework.project.application.port.in;
 
 public interface ProjectLikeCommandUseCase {
 	void like(Long userId, Long projectId);
+    void delete(Long userId, Long projectId);
 }
