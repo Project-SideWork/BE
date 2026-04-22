95cb6d9a5cd7e7a8607f59b45930169b8bf35f3e
Jinyoung <glitt5384@naver.com>
2026-04-22 16:58:02 +0900

    feat: 프로젝트 좋아요 취소 로직의 성공 및 실패 테스트

--- main-modules/project/application/src/test/java/com/sidework/project/application/ProjectLikeCommandServiceTest.java
+++ main-modules/project/application/src/test/java/com/sidework/project/application/ProjectLikeCommandServiceTest.java
@@ -1,5 +1,7 @@
 package com.sidework.project.application;
 
+import com.sidework.project.application.exception.ProjectLikeExistException;
+import com.sidework.project.application.exception.ProjectLikeNotFoundException;
 import com.sidework.project.application.exception.ProjectNotFoundException;
 import com.sidework.project.application.port.in.ProjectQueryUseCase;
 import com.sidework.project.application.port.out.ProjectLikeOutPort;
@@ -74,4 +76,59 @@ class ProjectLikeCommandServiceTest {
 		verify(userQueryUseCase, never()).validateExists(any());
 		verify(projectLikeOutPort, never()).like(any(ProjectLike.class));
 	}
+
+    @Test
+    void 이미_좋아요한_프로젝트면_ProjectLikeExistException을_던진다() {
+        // given
+        Long userId = 1L;
+        Long projectId = 2L;
+
+        when(projectQueryUseCase.queryById(projectId))
+                .thenReturn(Project.builder().id(projectId).build());
+        doNothing().when(userQueryUseCase).validateExists(userId);
+        when(projectLikeOutPort.isLiked(userId, projectId)).thenReturn(true);
+
+        // when & then
+        assertThrows(
+                ProjectLikeExistException.class,
+                () -> service.like(userId, projectId)
+        );
+
+        verify(projectQueryUseCase).queryById(projectId);
+        verify(userQueryUseCase).validateExists(userId);
+        verify(projectLikeOutPort).isLiked(userId, projectId);
+        verify(projectLikeOutPort, never()).like(any(ProjectLike.class));
+    }
+
+    @Test
+    void delete는_userId와_projectId로_unlike를_호출한다() {
+        // given
+        Long userId = 10L;
+        Long projectId = 10L;
+        when(projectLikeOutPort.isLiked(userId, projectId)).thenReturn(true);
+        doNothing().when(projectLikeOutPort).unlike(userId, projectId);
+
+        // when
+        service.delete(userId, projectId);
+
+        // then
+        verify(projectLikeOutPort).isLiked(userId, projectId);
+        verify(projectLikeOutPort).unlike(userId, projectId);
+    }
+
+    @Test
+    void delete는_존재하지_않는_좋아요_내역을_삭제하려고하면_ProjectLikeNotFoundException을_던진다() {
+        // given
+        Long userId = 10L;
+        Long projectId = 10L;
+
+        when(projectLikeOutPort.isLiked(userId, projectId)).thenReturn(false);
+        assertThrows(
+                ProjectLikeNotFoundException.class,
+                () -> service.delete(userId, projectId)
+        );
+
+        // then
+        verify(projectLikeOutPort).isLiked(userId, projectId);
+    }
 }
