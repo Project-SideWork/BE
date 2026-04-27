da4bc25b8eda91dec7a17b9b27ea4adfc5df0f04
Jinyoung <glitt5384@naver.com>
2026-04-27 23:25:12 +0900

    feat: 마지막에 읽은 메시지 정보 업데이트

--- main-modules/chat/application/src/main/java/com/sidework/chat/application/port/out/ChatRoomOutPort.java
+++ main-modules/chat/application/src/main/java/com/sidework/chat/application/port/out/ChatRoomOutPort.java
@@ -2,7 +2,12 @@ package com.sidework.chat.application.port.out;
 
 import com.sidework.domain.ChatRoom;
 
+import java.time.LocalDateTime;
+
 public interface ChatRoomOutPort {
     Long save(ChatRoom chatRoom);
     boolean existsById(Long chatRoomId);
+    int updateChatRoomLatest(
+            String messageContent, LocalDateTime messageSendTime, Long lastMessageId, Long lastMessageSenderId
+    );
 }
--- main-modules/chat/infrastructure/src/main/java/com/sidework/chat/persistence/repository/ChatRoomJpaRepository.java
+++ main-modules/chat/infrastructure/src/main/java/com/sidework/chat/persistence/repository/ChatRoomJpaRepository.java
@@ -1,9 +1,29 @@
 package com.sidework.chat.persistence.repository;
 
 import com.sidework.chat.persistence.entity.ChatRoomEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
 
 @Repository
 public interface ChatRoomJpaRepository extends JpaRepository<ChatRoomEntity, Long> {
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(
+            """
+            UPDATE ChatRoomEntity cr
+            SET cr.lastMessageContent = :content, cr.lastMessageSentTime = :sendTime,
+            cr.lastMessageId = :lastMessageId, cr.lastMessageSenderId = :lastMessageSenderId
+            """
+    )
+    int updateLastMessage(
+            @Param("content") String messageContent,
+            @Param("sendTime") Instant messageSendTime,
+            @Param("lastMessageId") Long lastMessageId,
+            @Param("lastMessageSenderId")Long lastMessageSenderId
+    );
 }
