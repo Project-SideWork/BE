c2d3d7abd7ae543eacf31123dd18c01483f50a83
Jinyoung <glitt5384@naver.com>
2026-04-27 23:24:31 +0900

    feat: 메시지 생성 후 알림 이벤트 생성 및 사용자별 채팅방 조회 구현

--- main-modules/chat/application/src/main/java/com/sidework/chat/application/port/in/ChatQueryUseCase.java
+++ main-modules/chat/application/src/main/java/com/sidework/chat/application/port/in/ChatQueryUseCase.java
@@ -3,5 +3,6 @@ package com.sidework.chat.application.port.in;
 
 public interface ChatQueryUseCase {
     ChatMessageQueryResult queryMessagesByChatRoomId(Long chatRoomId, String cursor);
+    ChatRoomQueryResult queryRoomsByUserId(Long userId, String cursor);
     void checkChatUserValidation(Long userId, Long chatRoomId);
 }
--- main-modules/chat/application/src/main/java/com/sidework/chat/application/service/ChatCommandService.java
+++ main-modules/chat/application/src/main/java/com/sidework/chat/application/service/ChatCommandService.java
@@ -6,13 +6,22 @@ import com.sidework.chat.application.port.in.ExistChatCommand;
 import com.sidework.chat.application.port.out.ChatMessageOutPort;
 import com.sidework.chat.application.port.out.ChatRoomOutPort;
 import com.sidework.chat.application.port.out.ChatUserOutPort;
+import com.sidework.common.event.ChatMessageSendEvent;
+import com.sidework.common.event.sse.port.out.ChatMessageData;
 import com.sidework.common.event.sse.port.out.SseSendOutPort;
+import com.sidework.common.exception.ResourceUpdateFailedException;
+import com.sidework.common.response.status.ErrorStatus;
 import com.sidework.domain.ChatMessage;
 import com.sidework.domain.ChatRoom;
 import com.sidework.domain.ChatUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Service
 @RequiredArgsConstructor
@@ -22,25 +31,47 @@ public class ChatCommandService implements ChatCommandUseCase {
     private final ChatUserOutPort chatUserRepository;
     private final ChatRoomOutPort chatRoomRepository;
     private final SseSendOutPort sseSendAdapter;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Override
     public void processStartNewChat(Long senderId, NewChatCommand chatCommand) {
         Long newChatRoom = createNewChatRoom(chatCommand.content());
-        Long messageId = createNewChatMessage(newChatRoom, senderId, chatCommand.content());
+        LocalDateTime sendTime = Instant.now()
+                .atZone(ZoneId.of("Asia/Seoul"))
+                .toLocalDateTime();
+        Long messageId = createNewChatMessage(newChatRoom, senderId, chatCommand.content(), sendTime);
 
         createNewChatUser(newChatRoom, senderId, messageId);
         createNewChatUser(newChatRoom, chatCommand.receiverId(), null);
 
+        if(chatRoomRepository.updateChatRoomLatest(chatCommand.content(), sendTime, messageId, senderId) == 0) {
+            throw new ResourceUpdateFailedException(ErrorStatus.CHATROOM_UPDATE_ERROR);
+        }
+
         sseSendAdapter.sendToUser(chatCommand.receiverId(), chatCommand.content());
+        eventPublisher.publishEvent(new ChatMessageSendEvent(chatCommand.receiverId(), chatCommand.content()));
     }
 
     @Override
-    public void processResumeChat(Long chatRoomId, Long senderId,ExistChatCommand chatCommand) {
-        Long messageId = createNewChatMessage(chatRoomId, senderId, chatCommand.content());
-        chatUserRepository.updateLastReadChat(senderId, chatRoomId, messageId);
+    public void processResumeChat(Long chatRoomId, Long senderId, ExistChatCommand chatCommand) {
+        LocalDateTime sendTime = Instant.now()
+                .atZone(ZoneId.of("Asia/Seoul"))
+                .toLocalDateTime();
+        Long messageId = createNewChatMessage(chatRoomId, senderId, chatCommand.content(), sendTime);
+        Long pairUserId = chatUserRepository.findChatPairInRoom(senderId, chatRoomId);
 
-        // TODO: 사용자가 채팅방에 없는 경우에는 알림함으로 보내게 할 수 있도록 판단하는 로직 추가 필요.
-        sseSendAdapter.sendToChatRoom(chatRoomId, chatCommand.content());
+        if(chatRoomRepository.updateChatRoomLatest(chatCommand.content(), sendTime, messageId, senderId) == 0) {
+            throw new ResourceUpdateFailedException(ErrorStatus.CHATROOM_UPDATE_ERROR);
+        }
+
+        if(chatUserRepository.updateLastReadChat(senderId, chatRoomId, messageId) == 0) {
+            throw new ResourceUpdateFailedException(ErrorStatus.CHATUSER_UPDATE_ERROR);
+        }
+
+        sseSendAdapter.sendToChatRoom(chatRoomId, new ChatMessageData(messageId, chatCommand.content(), sendTime.toString(), senderId));
+        if(chatUserRepository.existsByUserAndRoom(pairUserId, chatRoomId)){
+            eventPublisher.publishEvent(new ChatMessageSendEvent(pairUserId, chatCommand.content()));
+        }
     }
 
     public Long createNewChatRoom(String messageContent) {
@@ -48,13 +79,18 @@ public class ChatCommandService implements ChatCommandUseCase {
         return chatRoomRepository.save(chatRoom); // 저장된 채팅방의 ID
     }
 
-    public Long createNewChatMessage(Long chatRoomId, Long senderId, String content) {
-        ChatMessage chatMessage = ChatMessage.create(chatRoomId, senderId, content);
+    public Long createNewChatMessage(Long chatRoomId, Long senderId, String content, LocalDateTime sendTime) {
+        ChatMessage chatMessage = ChatMessage.create(chatRoomId, senderId, content, sendTime);
         return chatMessageRepository.save(chatMessage); // 저장된 채팅 메시지의 ID
     }
 
     public void createNewChatUser(Long chatRoomId, Long userId, Long lastReadChatId) {
         ChatUser chatUser = ChatUser.create(chatRoomId, userId, lastReadChatId);
         chatUserRepository.save(chatUser);
+    }
+
+    public void updateLastMessage(ChatRoom chatRoom, Long lastMessageId, Long lastMessageSenderId) {
+        chatRoom.changeLastMessage(lastMessageId, lastMessageSenderId);
+        chatRoomRepository.save(chatRoom);
     }
 }
