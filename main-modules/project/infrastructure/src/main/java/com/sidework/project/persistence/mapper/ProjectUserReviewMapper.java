7207ad27bcf439872076e42b232457ce1ea8f887
Jinyoung <glitt5384@naver.com>
2026-05-07 12:16:07 +0900

    feat: instant <-> localdate

--- main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/mapper/ProjectUserReviewMapper.java
+++ main-modules/project/infrastructure/src/main/java/com/sidework/project/persistence/mapper/ProjectUserReviewMapper.java
@@ -5,8 +5,28 @@ import org.mapstruct.Mapper;
 import com.sidework.project.domain.ProjectUserReview;
 import com.sidework.project.persistence.entity.ProjectUserReviewEntity;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 @Mapper(componentModel = "spring")
 public interface ProjectUserReviewMapper {
+    ZoneId PROJECT_ZONE = ZoneId.of("Asia/Seoul");
+
 	ProjectUserReview toDomain(ProjectUserReviewEntity entity);
 	ProjectUserReviewEntity toEntity(ProjectUserReview domain);
+
+    default Instant map(LocalDate value) {
+        if (value == null) return null;
+        return value
+                .atStartOfDay(PROJECT_ZONE)
+                .toInstant();
+    }
+
+    default LocalDate map(Instant value) {
+        if (value == null) return null;
+        return value
+                .atZone(PROJECT_ZONE)
+                .toLocalDate();
+    }
 }
