8ce640fd7b245a96d23c0fce844e0c84e171135f
Jinyoung <glitt5384@naver.com>
2026-05-07 12:14:19 +0900

    feat: 사용자별 리뷰 조회 구현 및 테스트

--- main-modules/project/application/src/main/java/com/sidework/project/application/port/in/ProjectQueryUseCase.java
+++ main-modules/project/application/src/main/java/com/sidework/project/application/port/in/ProjectQueryUseCase.java
@@ -4,6 +4,7 @@ import com.sidework.common.response.PageResponse;
 import com.sidework.project.application.adapter.MyProjectSummaryResponse;
 import com.sidework.project.application.adapter.ProjectDetailResponse;
 import com.sidework.project.application.adapter.ProjectListResponse;
+import com.sidework.project.application.dto.ProjectIdTitleProjection;
 import com.sidework.project.application.dto.ProjectUserReviewStatSummary;
 import com.sidework.project.application.dto.ProjectUserReviewSummary;
 import com.sidework.project.domain.Project;
@@ -18,7 +19,6 @@ import java.util.Map;
 
 public interface ProjectQueryUseCase {
     Project queryById(Long projectId);
-    List<Project> queryByUserId(Long userId); // TODO: 페이지네이션 적용 이후 삭제
     List<Project> pageByUserId(Long userId, Pageable pageable);
     ProjectDetailResponse queryProjectDetail(Long userId, Long projectId);
     List<ProjectRecruitPosition> queryProjectRecruitPosition(Long projectId);
@@ -27,8 +27,10 @@ public interface ProjectQueryUseCase {
     PageResponse<List<ProjectListResponse>> queryProjectList(Long userId, String keyword, List<Long> skillIds, Pageable pageable);
 	PageResponse<List<ProjectListResponse>> queryLikedProjectList(Long userId, String keyword, List<Long> skillIds, Pageable pageable);
     ProjectUserReviewStatSummary queryStatSummaryByUserId(Long userId);
-    List<ProjectUserReviewSummary> queryReviewSummaryByProjectIds(Long userId, List<Long> projectIds);
+    List<ProjectUserReviewSummary> queryReviewSummaryByUserId(Long userId, Pageable pageable);
     Map<Long, Double> queryAverageReviewScoresByUserIds(List<Long> userIds);
     List<MyProjectSummaryResponse> queryMyProjectSummary(Long userId);
     Long queryProjectCount(Long userId);
+    Long queryReviewCount(Long userId);
+    List<ProjectIdTitleProjection> queryUserProjectIdTitlePairs(List<Long> projectIds);
 }
